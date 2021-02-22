e794a8a8b58787b01f6753a85279ce12f196315d
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-02-22 14:36:03 +0330

    Update startup.sh

--- resources/startup/startup.sh
+++ resources/startup/startup.sh
@@ -1,18 +1,10 @@
-#!/bin/bash
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
- 
+
 echo "
  
-        +-+ +-+ +-+ +-+ +-+ +-+ +-+
-        |U| |L| |T| |R| |O| |I| |D|
-        +-+ +-+ +-+ +-+ +-+ +-+ +-+
+        +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+
+        |M| |E| |G| |A| |T| |R| |O| |N|
+        +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+
  
- 
-      Visit @TheUltroid for updates!!
 
 "
  
