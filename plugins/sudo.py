5a7bac0a317fa3c25d6fa06bd8ed12a6471b8d01
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-03-05 18:03:17 +0330

    Update sudo.py

--- plugins/sudo.py
+++ plugins/sudo.py
@@ -29,6 +29,17 @@ from . import *
     pattern="addsudo ?(.*)",
 )
 async def _(ult):
+    if Var.BOT_MODE == True:
+        try:
+            if ult.sender_id != Var.OWNER_ID:
+                return await eor(
+                    ult, "You are sudo user, You cant add other sudo users."
+                )
+        except BaseException:
+            pass
+    else:
+        if ult.sender_id != ultroid_bot.uid:
+            return await eor(ult, "You are sudo user, You cant add other sudo users.")
     ok = await eor(ult, "`Updating SUDO Users List ...`")
 
     if ult.reply_to_msg_id:
@@ -101,6 +112,17 @@ async def _(ult):
     pattern="delsudo ?(.*)",
 )
 async def _(ult):
+    if Var.BOT_MODE == True:
+        try:
+            if ult.sender_id != Var.OWNER_ID:
+                return await eor(
+                    ult, "You are sudo user, You cant add other sudo users."
+                )
+        except BaseException:
+            pass
+    else:
+        if ult.sender_id != ultroid_bot.uid:
+            return await eor(ult, "You are sudo user, You cant add other sudo users.")
     ok = await eor(ult, "`Updating SUDO Users List ...`")
 
     if ult.reply_to_msg_id:
@@ -191,4 +213,4 @@ async def _(ult):
     return await ok.edit(f"**List of SUDO Users :**\n{msg}")
 
 
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=Var.HNDLR)}"})
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
