e1e36b99fb3165ed6fa5739a07cd24a5b3cf8470
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-03-17 19:19:29 +0330

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -18,7 +18,7 @@ from . import *
 @asst_cmd("start")
 async def assistant(event):
     if event.is_group and event.sender_id in sed:
-        return await eor(event, "`I dont work in groups`")
+        return await eor(event, "`I don't work in groups`")
     else:
         if not is_added(event.sender_id) and event.sender_id not in sed:
             add_user(event.sender_id)
@@ -33,9 +33,28 @@ async def assistant(event):
             return
         await event.reply(
             f"Ⲏⲉⲩ, ⲧⲏⲓⲋ ⲓⲋ Ⲋυⲣⲉʀ Ⲧⲉⲥⲏⲛⲟⳑⲟⳋⲩ Ⲁⲋⲋⲓⲋⲧⲁⲛⲧ ⲟϝ {OWNER_NAME}!\n\n{ok}",
-            buttons=[Button.url("Ⳗⲟⲓⲛ Ⲙⲩ Ⲥⲏⲁⲛⲛⲉⳑ", url="https://t.me/FutureTechnologyGuardX")],
+            buttons=[
+                [
+                    Button.url("Jᴏin Chᴀnnᴇl", url="https://t.me/FutureTechnologyGuardX"),
+                ],
+                [
+                    Button.url("CɪᴘʜᴇʀX Sᴇrvᴇr Ping", url="https://t.me/CipherXBot?start=ping"),
+                ],
+                [
+                    Button.inline("Tᴇxᴛ Trᴀnslᴀᴛᴏr", data="trans"),
+                ],
+                [
+                    Button.inline("CɪᴘʜᴇʀX Bᴏᴛs Lisᴛ", data="list"),
+                ],
         )
 
+@callback("trans")
+async def trans(event):
+    await event.edit(event.chat_id, "How to use:\n1) /tr LangaugeCode | text to translate \n2) `/tr Language Code` as reply to a message \n\nHere is the list of [Language Codes](https://telegra.ph/CɪᴘʜᴇʀX-03-10)")
+
+@callback("list")
+async def list(event):
+    await event.edit(event.chat_id, "⚜️ Here is the list of bots made by CɪᴘʜᴇʀX ⚜️\n\n🎇 CɪᴘʜᴇʀX Assistant ~ [Bot Link](https://t.me/CipherXBot)\n\n🎇 File to Link Generator ~ [Bot Link](https://t.me/FiletoLinkTelegramBot)\n\n🎇 Rename with Custom Thumbnail ~ [Bot Link](https://t.me/RenameTelegramBot)\n\n🎇 InvisibleX Group Manager ~ [Bot Link](https://t.me/InvisibleXBot)\n\n🎇 Future Technology Chat Bot ~ [Bot Link](https://t.me/FutureTechnologyChatBot)")
 
 @asst_cmd("start")
 @owner
