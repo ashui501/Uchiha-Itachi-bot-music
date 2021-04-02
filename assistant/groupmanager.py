2405ca1078a875134166dedd4090a9f24ae7e1b3
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-04-02 16:15:37 +0430

    Update groupmanager.py

--- assistant/groupmanager.py
+++ assistant/groupmanager.py
@@ -265,7 +265,7 @@ async def pin(event):
 
 async def get_user_from_event(event):
     """ Get the user from argument or replied message. """
-    args = event.pattern_match.group(1).split(" ", 1)
+    args = event.pattern_match.group(1)
     extra = None
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
