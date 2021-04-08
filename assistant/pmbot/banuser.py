3bad7d3ff3c13cc834363aa322784bd0e910c9c5
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-04-09 02:06:28 +0430

    Update banuser.py

--- assistant/pmbot/banuser.py
+++ assistant/pmbot/banuser.py
@@ -11,8 +11,8 @@ async def banhammer(event):
         blacklist_user(target)
         await asst.send_message(event.chat_id, f"#BAN\nUser - {target}")
         await asst.send_message(
-            target,
-            "`GoodBye! You have been banned.`\n**Further messages you send will not be forwarded.**",
+            target
+            "`GoodBye! You have been banned bitch.`\n**Further messages you send will not be forwarded.**\n\n**✨ CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ Ⲃⲟⲧ ✨**",
         )
     else:
         return await asst.send_message(event.chat_id, f"User is already banned!")
