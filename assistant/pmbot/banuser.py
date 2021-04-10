c1c5cbc54f2cb789b4af5702a44f352aa9b7ab7d
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-04-10 15:24:52 +0430

    Update banuser.py

--- assistant/pmbot/banuser.py
+++ assistant/pmbot/banuser.py
@@ -12,7 +12,7 @@ async def banhammer(event):
         await asst.send_message(event.chat_id, f"#BAN\nUser - {target}")
         await asst.send_message(
             target
-            "`GoodBye! You have been banned 😒`\n**Further messages you send will not be forwarded.**",
+            "`GoodBye! You have been banned.`\n**Further messages you send will not be forwarded.**",
         )
     else:
         return await asst.send_message(event.chat_id, f"User is already banned!")
