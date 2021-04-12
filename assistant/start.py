94102d38c44292a1ff73571e5b9cbfe287f871c6
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-04-12 10:09:34 +0430

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -24,7 +24,7 @@ async def assistant(event):
     if event.is_group and event.sender_id in sed:
         bnn = (await asst.get_me()).username
         return await event.reply(
-            "`I dont work in groups`",
+            "`Click the button below to see my full commands`",
             buttons=[Button.url("⚙️Sᴛᴀʀᴛ⚙️", url=f"https://t.me/{bnn}?start=set")],
         )
     else:
