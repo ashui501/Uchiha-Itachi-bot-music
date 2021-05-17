9fed525f8a657158701472a797f98eace03dd8d4
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-05-17 10:16:05 +0430

    Update translate.py

--- assistant/translate.py
+++ assistant/translate.py
@@ -1,9 +1,10 @@
 import emoji
-from . import *
+import requests
 from google_trans_new import google_translator
 from googletrans import LANGUAGES
 from langdetect import detect
 
+from . import *
 
 @asst_cmd("tr") 
 async def _(event):
@@ -27,14 +28,26 @@ async def _(event):
     after_tr_text = lmao
     source_lan = LANGUAGES[after_tr_text]
     transl_lan = LANGUAGES[lan]
-    output_str = f"""**Trᴀnslᴀᴛᴇd by CɪᴘʜᴇʀX Bᴏᴛ**
-**Source ({source_lan})**:
+    output_str = f"""**Ⲧʀⲁⲛⲋⳑⲁⲧⲉⲇ ⲃⲩ CɪᴘʜᴇʀX Bᴏᴛ**
+**Ⲋⲟυʀⲥⲉ ({source_lan})**:
 `{text}`
 
-**Translation ({transl_lan})**:
+**Ⲧʀⲁⲛⲋⳑⲁⲧⲓⲟⲛ ({transl_lan})**:
 `{translated}`"""
 
     try:
         await asst.send_message(event.chat_id, output_str)
+        if len(tr_text) >= 4096:
+            url = "https://del.dog/documents"
+            r = requests.post(url, data=tr_text.encode("UTF-8")).json()
+            url2 = f"https://del.dog/{r['key']}"
+            tr_text = (
+                f"Translated text was too big, so I've pasted it [Here]({url2})"
+            )
+    await event.edit(tr_text)
     except Exception:
         await asst.send_message(event.chat_id, "Something went wrong 🤔\nSee [Language Codes](https://telegra.ph/CɪᴘʜᴇʀX-03-10) and try again.")
+        
+        
+        
+        
