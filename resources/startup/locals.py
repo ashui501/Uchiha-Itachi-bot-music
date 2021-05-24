e31c526dbdd92c24f0d08977fd08924c6b235250
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-05-24 11:20:43 +0430

    Update locals.py

--- resources/startup/locals.py
+++ resources/startup/locals.py
@@ -29,11 +29,12 @@ def start():
     print("Welcome to CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ, lets start setting up!\n\n")
     print("Cloning the repository...\n\n")
     try:
-        os.system("git clone https://github.com/CipherX1-ops/Megatron && cd Megatron")
-    except Exception as e:
-        print(f"ERROR\n{str(e)}")
+        os.system("rm -rf Megatron")
+    except BaseException:
+        pass
+    os.system("git clone https://github.com/CipherX1-ops/Megatron")
     print("\n\nDone")
-    os.system("cd Megatron")
+    os.chdir("Megatron")
     clear_screen()
     print(a)
     print("\n\nLet's start!\n")
@@ -58,11 +59,8 @@ def start():
         "API_ID",
         "API_HASH",
         "SESSION",
-        "BOT_USERNAME",
-        "BOT_TOKEN",
         "REDIS_URI",
         "REDIS_PASSWORD",
-        "LOG_CHANNEL",
     ]
     all_done = "# Megatron Environment Variables.\n# Do not delete this file.\n\n"
     for i in varrs:
@@ -88,6 +86,7 @@ def start():
     clear_screen()
     print("\nCongrats. All done!\nTime to start the CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ!")
     print("\nInstalling requirements... This might take a while...")
+    os.system("pip3 install -r requirements.txt")
     os.system("pip3 install -r resources/extras/local-requirements.txt")
     clear_screen()
     print(a)
