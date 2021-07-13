7c825947dfa70ef900111f994d4959d74ffaf7aa
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-14 00:19:00 +0430

    Update translate.py

--- assistant/translate.py
+++ assistant/translate.py
@@ -6,23 +6,24 @@ from . import *
 
 @asst_cmd("tr") 
 async def _(event):
+    if len(event.text) > 3:
+        if not event.text[3] == " ":
+            return
     input = event.text[4:6]
+    txt = event.text[7:]
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         text = previous_message.message
         lan = input or "en"
-    elif "|" in input:
-        lan, text = input.split("|")
+    elif input:
+        text = txt
+        lan = input or "en"
     else:
-        await asst.send_message(
-            event.chat_id, "`/tr LanguageCode` as reply to a message"
-        )
-        return
+        return await event.reply("1⃣ `/tr` [Language Code](https://telegra.ph/CɪᴘʜᴇʀX-03-10) as reply to a message\2⃣ `/tr` <target LangCode> <text> ~ Ex: /tr ko hello", link_preview=False)
     translator = Translator()
     try:
         tt = translator.translate(text, dest=lan)
-        output_str = f"""**Ⲧʀⲁⲛⲋⳑⲁⲧⲉⲇ ⲃⲩ CɪᴘʜᴇʀX Ⲃⲟⲧ**\n**Ⲋⲟυʀⲥⲉ ({tt.src})**:\n`{text}`\n\n**Ⲧʀⲁⲛⲋⳑⲁⲧⲓⲟⲛ ({transl_lan})**:\n`{lan}`"""
-        await asst.send_message(event.chat_id, output_str)
+        output_str = f"""**Ⲧʀⲁⲛⲋⳑⲁⲧⲉⲇ ⲃⲩ CɪᴘʜᴇʀX Ⲃⲟⲧ**\n**Ⲋⲟυʀⲥⲉ ({tt.src})**:\n`{text}`\n\n**Ⲧʀⲁⲛⲋⳑⲁⲧⲓⲟⲛ ({tt.text})**:\n`{lan}`"""
         if len(output_str) >= 4096:
             url = "https://del.dog/documents"
             r = requests.post(url, data=output_str.encode("UTF-8")).json()
@@ -30,6 +31,7 @@ async def _(event):
             output_str = (
                 f"Translated text was too big, so I've pasted it [Here]({url2})"
             )
+        await asst.send_message(event.chat_id, output_str, link_preview=False)
     except Exception:
         await asst.send_message(event.chat_id, "Something went wrong 🤔\nSee [Language Codes](https://telegra.ph/CɪᴘʜᴇʀX-03-10) and try again.", link_preview=False)
         
