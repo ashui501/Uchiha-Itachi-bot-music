2836730432d383010c793d8fd27c0b93db41331b
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-15 19:59:56 +0430

    Update updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -45,7 +45,7 @@ async def _(e):
     else:
         await eod(
             e,
-            f'<code>CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ </code><strong>up-to-date</strong>',
+            f'<code>CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ : </code><strong>｡⌬｡up-to-date｡⌬｡</strong>',
             parse_mode="html",
             time=5, 
             link_preview=False,
