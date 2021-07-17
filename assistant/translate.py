471ba27b4ca1545723afd9ceeb88bb2aac1a7f66
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-17 19:58:06 +0430

    Update translate.py

--- assistant/translate.py
+++ assistant/translate.py
@@ -1,17 +1,13 @@
+import requests
 from googletrans import Translator
 
 from . import *
 
 @asst_cmd("tr") 
 async def _(event):
-    if not event.is_private:
-        return
-    if not event.is_group:
-        return
     if len(event.text) > 3:
         if not event.text[3] == " ":
             return
-    await event.get_input_chat()
     input = event.text[4:6]
     txt = event.text[7:]
     if event.reply_to_msg_id:
@@ -27,13 +23,13 @@ async def _(event):
     try:
         tt = translator.translate(text, dest=lan)
         output_str = f"""**Ⲧʀⲁⲛⲋⳑⲁⲧⲉⲇ ⲃⲩ CɪᴘʜᴇʀX Ⲃⲟⲧ**\n\n**Ⲋⲟυʀⲥⲉ ({tt.src})**:\n`{text}`\n\n**Ⲧʀⲁⲛⲋⳑⲁⲧⲓⲟⲛ ({lan})**:\n`{tt.text}`"""
-        #if len(output_str) >= 4096:
-            #url = "https://del.dog/documents"
-            #r = requests.post(url, data=output_str.encode("UTF-8")).json()
-            #url2 = f"https://del.dog/{r['key']}"
-            #output_str = (
-            #    f"Translated text was too big, so I've pasted it [Here]({url2})"
-            #)
+        if len(output_str) >= 4096:
+            url = "https://del.dog/documents"
+            r = requests.post(url, data=output_str.encode("UTF-8")).json()
+            url2 = f"https://del.dog/{r['key']}"
+            output_str = (
+                f"Translated text was too big, so I've pasted it [Here]({url2})"
+            )
         await asst.send_message(event.chat_id, output_str)
     except Exception:
         await asst.send_message(event.chat_id, "Something went wrong 🤔\nSee [Language Codes](https://telegra.ph/CɪᴘʜᴇʀX-03-10) and try again.", link_preview=False)
