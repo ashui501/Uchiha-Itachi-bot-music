017b3383f11b294b3afce7ee2192163a86aaa06a
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-18 23:09:40 +0430

    Update ping.py

--- assistant/ping.py
+++ assistant/ping.py
@@ -12,12 +12,12 @@ from datetime import datetime
 
 Lastupdate = time.time()
 
-@asst_cmd("ping")
+@asst_cmd("ping$")
 async def _(event):
     start = datetime.now()
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await asst.send_message(
         event.chat_id,
-        f"**█▀█ █ █▄░█ █▀▀ █ \n█▀▀ █ █░▀█ █▄█ ▄**\n\n ➲CɪᴘʜᴇʀX Ⲋⲉʀⳳⲉʀ Ⲣⲓⲛⳋ~`{ms}ms`",
+        f"**█▀█ █ █▄░█ █▀▀ █ \n█▀▀ █ █░▀█ █▄█ ▄**\n\n ➲CɪᴘʜᴇʀX Ⲋⲉʀⳳⲉʀ Ⲣⲓⲛⳋ~`{ms} milliseconds`",
     )
