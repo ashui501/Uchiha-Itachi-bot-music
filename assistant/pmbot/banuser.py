25553b6c9af4408d9bf3746c2d7c8d8849e22124
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-18 22:32:18 +0430

    Update banuser.py

--- assistant/pmbot/banuser.py
+++ assistant/pmbot/banuser.py
@@ -2,7 +2,10 @@ from . import *
 
 
 @asst_cmd("ban")
+@owner
 async def banhammer(event):
+    if not event.is_private:
+        return
     x = await event.get_reply_message()
     if x is None:
         return await event.edit("Please reply to someone to ban him.")
@@ -19,7 +22,10 @@ async def banhammer(event):
 
 
 @asst_cmd("unban")
+@owner
 async def banhammer(event):
+    if not event.is_private:
+        return
     x = await event.get_reply_message()
     if x is None:
         return await event.edit("Please reply to someone to ban him.")
