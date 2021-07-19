b5cefa9e46a8a168c3640a4fcde25e559b4fb1ff
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-19 17:23:11 +0430

    Update clean_action.py

--- plugins/clean_action.py
+++ plugins/clean_action.py
@@ -1,5 +1,5 @@
 # Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
+# Copyright (C) 2021 TeamUltroid
 #
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
@@ -9,13 +9,13 @@
 ✘ Commands Available -
 
 •`{i}addclean`
-    Clean all Upcoming action messages in added chat like someone joined/left/pin etc.
+    Clean all Upcoming action msg in added chat like someone joined/left/pin etc.
 
-•`{i}rmclean`
+•`{i}remclean`
     Remove chat from database.
 
 •`{i}listclean`
-   To get list of all chats where it's activated.
+   To get list of all chats where its activated.
 
 """
 
@@ -28,12 +28,9 @@ from . import *
 async def _(e):
     add_clean(e.chat_id)
     await eod(e, "Added Clean Action Setting For this Chat")
-    async for x in ultroid_bot.iter_messages(e.chat_id, limit=3000):
-        if x.action:
-            await x.delete()
 
 
-@ultroid_cmd(pattern="rmclean$")
+@ultroid_cmd(pattern="remclean$")
 async def _(e):
     rem_clean(e.chat_id)
     await eod(e, "Removed Clean Action Setting For this Chat")
@@ -47,22 +44,10 @@ async def _(e):
         o = ""
         for x in k:
             try:
-                title = (await ultroid_bot.get_entity(int(x))).title
+                title = e.chat.title
             except BaseException:
                 title = "`Invalid ID`"
             o += x + " " + title + "\n"
         await eor(e, o)
     else:
         await eod(e, "`No Chat Added`")
-
-
-@ultroid_bot.on(events.ChatAction())
-async def _(event):
-    if is_clean_added(event.chat_id):
-        try:
-            await event.delete()
-        except BaseException:
-            pass
-
-
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
