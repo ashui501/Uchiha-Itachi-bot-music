e88a02098004276bb67e577110917b30090dc794
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-19 17:35:39 +0430

    Update dm.py

--- plugins/dm.py
+++ plugins/dm.py
@@ -1,20 +1,22 @@
 # Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
+# Copyright (C) 2021 TeamUltroid
 #
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 """
 ✘ Commands Available -
+
 • `{i}dm <username/id> <reply/type>`
     Direct Message the User.
 """
-
 from . import *
 
 
 @ultroid_cmd(pattern="dm ?(.*)")
 async def dm(e):
+    if not e.out and not is_fullsudo(e.sender_id):
+        return await eor(e, "`This Command is Full Sudo Restricted.`")
     if len(e.text) > 3:
         if not e.text[3] == " ":  # weird fix
             return
@@ -23,25 +25,18 @@ async def dm(e):
     try:
         chat_id = await get_user_id(c[0])
     except Exception as ex:
-        return await eod(e, "`" + str(ex) + "`", time=5)
+        return await eod(e, "`" + str(ex) + "`")
     msg = ""
     masg = await e.get_reply_message()
     if e.reply_to_msg_id:
-        await ultroid_bot.send_message(chat_id, masg)
-        await eod(e, "`⚜️Message Delivered!`", time=4)
+        await e.client.send_message(chat_id, masg)
+        await eod(e, "`⚜️Message Delivered!`")
     for i in c[1:]:
         msg += i + " "
     if msg == "":
         return
     try:
-        await ultroid_bot.send_message(chat_id, msg)
-        await eod(e, "`⚜️Message Delivered!⚜️`", time=4)
+        await e.client.send_message(chat_id, msg)
+        await eod(e, "`⚜️Message Delivered!⚜️`")
     except BaseException:
-        await eod(
-            e,
-            "`{i}help dm`",
-            time=4,
-        )
-
-
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
+        await eod(e, f"Read Usage : `{HNDLR}help dm`")
