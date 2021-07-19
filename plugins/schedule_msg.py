0033bae900a938cfee9471cdb0ab6b1ffd15418e
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-19 18:33:42 +0430

    Update schedule_msg.py

--- plugins/schedule_msg.py
+++ plugins/schedule_msg.py
@@ -1,19 +1,17 @@
 # Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
+# Copyright (C) 2021 TeamUltroid
 #
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
 •`{i}schedule <text/reply to msg> <time>`
-    In time you can use second as number, or like 1h or 1m
-    eg. `{i}schedule Hello 100` It deliver message after 100 sec.
-    eg. `{i}schedule Hello 1h` It deliver message after an hour.
+    In time u can use second as number, or like 1h or 1m
+    eg. `{i}schedule Hello 100` It deliver msg after 100 sec.
+    eg. `{i}schedule Hello 1h` It deliver msg after an hour.
 """
-
 from datetime import timedelta
 
 from . import *
@@ -21,21 +19,23 @@ from . import *
 
 @ultroid_cmd(pattern="schedule ?(.*)")
 async def _(e):
+    if not e.out and not is_fullsudo(e.sender_id):
+        return await eod(e, "`This Command is Full Sudo Restricted`")
     x = e.pattern_match.group(1)
     xx = await e.get_reply_message()
     if x and not xx:
         y = x.split(" ")[-1]
         k = x.replace(y, "")
         if y.isdigit():
-            await ultroid_bot.send_message(
+            await e.client.send_message(
                 e.chat_id, k, schedule=timedelta(seconds=int(y))
             )
-            await eod(e, "`Scheduled message Succesfully`")
+            await eod(e, "`Scheduled msg Succesfully`")
         else:
             try:
                 z = await ban_time(e, y)
-                await ultroid_bot.send_message(e.chat_id, k, schedule=z)
-                await eod(e, "`Scheduled message Succesfully`")
+                await e.client.send_message(e.chat_id, k, schedule=z)
+                await eod(e, "`Scheduled msg Succesfully`")
             except BaseException:
                 await eod(e, "`Incorrect Format`")
     elif xx and x:
@@ -43,16 +43,13 @@ async def _(e):
             await e.client.send_message(
                 e.chat_id, xx, schedule=timedelta(seconds=int(x))
             )
-            await eod(e, "`Scheduled message Succesfully`")
+            await eod(e, "`Scheduled msg Succesfully`")
         else:
             try:
                 z = await ban_time(e, x)
-                await ultroid_bot.send_message(e.chat_id, xx, schedule=z)
-                await eod(e, "`Scheduled message Succesfully`")
+                await e.client.send_message(e.chat_id, xx, schedule=z)
+                await eod(e, "`Scheduled msg Succesfully`")
             except BaseException:
                 await eod(e, "`Incorrect Format`")
     else:
         return await eod(e, "`Incorrect Format`")
-
-
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
