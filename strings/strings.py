c6692698f09dbbf95d03875a61bfba9bee035b02
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-10-28 16:18:34 +0330

    Update strings.py

--- strings/strings.py
+++ strings/strings.py
@@ -1,13 +1,15 @@
 from os import listdir, path
 from typing import Any, Dict, List, Union
 
-from yaml import safe_load
-
+from google_trans_new import google_translator
 from cython import udB
-
 from yaml import safe_load
 
+language = [udB.get("language") or "en"]
 languages = {}
+
+Trs = google_translator()
+
 strings_folder = path.join(path.dirname(path.realpath(__file__)), "strings")
 
 for file in listdir(strings_folder):
@@ -19,13 +21,19 @@ for file in listdir(strings_folder):
 
 
 def get_string(key: str) -> Any:
+    lang = language[0]
     try:
-        return languages[(udB.get("language") or "en")][key]
+        return languages[lang][key]
     except KeyError:
         try:
-            return languages["en"][key]
+            tr = Trs.translate(languages["en"][key], lang_tgt=lang)
+            if languages.get(lang):
+                languages[lang][key] = tr
+            else:
+                languages.update({lang:{key:tr}})
+            return tr
         except KeyError:
-            return f"Warning: could not load any string with the key {key}"
+            return f"Warning: could not load any string with the key `{key}`"
 
 
 def get_languages() -> Dict[str, Union[str, List[str]]]:
