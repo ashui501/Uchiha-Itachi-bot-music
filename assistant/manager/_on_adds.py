1a7cb95911df6e6dc925a589f8c0c80628b4787f
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-10-28 17:14:45 +0330

    Create _on_adds.py

---
+++ assistant/manager/_on_adds.py
@@ -0,0 +1,15 @@
+from . import *
+from telethon import events
+
+
+@asst.on(events.ChatAction(func= lambda x: x.user_added))
+async def dueha(e):
+    user = await e.get_user()
+    if not user.is_self:
+        return
+    sm = udB.get("ON_MNGR_ADD")
+    if sm == "OFF":
+        return
+    if not sm:
+        sm = "Thanks for Adding me :)"
+    await e.reply(sm, link_preview=False)
