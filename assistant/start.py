d74a0601fc7ecf1b70cf7ec175190d0d1b4fbf7c
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-10-31 19:53:08 +0330

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -6,15 +6,17 @@
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 from datetime import datetime
-
 from pytz import timezone as tz
-from cython.functions.asst_fns import *
+
+from cython.dB.asst_fns import *
+from cython.dB.sudos import is_fullsudo
+from cython.functions.helper import inline_mention
 from cython.misc import owner_and_sudos
-from telethon import events
+
+from telethon import Button, events
 from telethon.utils import get_display_name
 
-from plugins import *
-
+from strings.strings import get_string
 from . import *
 
 
@@ -44,99 +46,106 @@ _start = [
     [Button.inline("TɪᴍᴇZᴏɴᴇ 🌎", data="tz")],
 ]
 
-@asst_cmd("start ?(.*)")
+@asst_cmd(pattern="start ?(.*)", forwards=False, func=lambda x: not x.is_group)
 async def ultroid(event):
-    if event.is_group:
-        return
-    else:
-        if (
-            not is_added(event.sender_id)
-            and str(event.sender_id) not in owner_and_sudos()
-        ):
-            add_user(event.sender_id)
-            await asst.send_message(
-                1601105531,
-                f"Bot started by [{event.sender_id}](tg://user?id={event.sender_id})",
+    if not is_added(event.sender_id) and str(event.sender_id) not in owner_and_sudos():
+        add_user(event.sender_id)
+        kak_uiw = udB.get("OFF_START_LOG")
+        if not kak_uiw or kak_uiw != "True":
+            msg = f"{inline_mention(event.sender)} `[{event.sender_id}]` started [Bot](@{asst.me.username})."
+            buttons = [[Button.inline("Info", "itkkstyo")]]
+            if event.sender.username:
+                buttons[0].append(Button.url("User", "t.me/" + event.sender.username))
+            await event.client.send_message(
+                int(udB["LOG_CHANNEL"]), msg, buttons=buttons
             )
-        if str(event.sender_id) not in owner_and_sudos():
-            ok = ""
-            u = await event.client.get_entity(event.chat_id)
-            if not udB.get("STARTMSG"):
-                if udB.get("PMBOT") == "True":
-                    ok = "✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵"
-                await event.reply(
-                    f"Ⲏⲉⲩ [{get_display_name(u)}](tg://user?id={u.id}), ⲧⲏⲓⲋ ⲓⲋ Ⲋυⲣⲉʀ Ⲧⲉⲥⲏⲛⲟⳑⲟⳋⲩ Ⲁⲋⲋⲓⲋⲧⲁⲛⲧ ⲟϝ {ultroid_bot.me.first_name}!\n\n{ok}",
-                    file=udB.get("STARTMEDIA"),
-                    buttons=[
-                        [
-                            Button.url("✵Jᴏin Chᴀnnᴇl✵", url="https://t.me/FutureTechnologyOfficial"),
-                        ],
-                        [
-                            Button.inline("✵Chᴀᴛ wiᴛh CɪᴘʜᴇʀX✵", data="chat"),
-                        ],
-                        [
-                            Button.inline("✵Grᴏuᴩ/Chᴀnnᴇl ʍᴀnᴀgᴇr Hᴇlᴩ✵", data="group"),
-                        ],
-                        [
-                            Button.inline("✵CɪᴘʜᴇʀX Sᴇrvᴇr Ping✵", data="ping"),
-                        ],
-                        [
-                            Button.inline("✵Tᴇxᴛ Trᴀnslᴀᴛᴏr✵", data="tlans"),
-                        ],
-                        [
-                            Button.inline("✵CɪᴘʜᴇʀX Bᴏᴛs Lisᴛ✵", data="list"),
-                        ],
+    if (event.sender_id != OWNER_ID) and not is_fullsudo(event.sender_id):
+        ok = ""
+        u = await event.client.get_entity(event.chat_id)
+        if not udB.get("STARTMSG"):
+            if udB.get("PMBOT") == "True":
+                ok = "✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵✵"
+            await event.reply(
+                f"Ⲏⲉⲩ [{get_display_name(u)}](tg://user?id={u.id}), ⲧⲏⲓⲋ ⲓⲋ Ⲋυⲣⲉʀ Ⲧⲉⲥⲏⲛⲟⳑⲟⳋⲩ Ⲁⲋⲋⲓⲋⲧⲁⲛⲧ ⲟϝ {ultroid_bot.me.first_name}!\n\n{ok}",
+                file=udB.get("STARTMEDIA"),
+                buttons=[
+                    [
+                        Button.url("✵Jᴏin Chᴀnnᴇl✵", url="https://t.me/FutureTechnologyOfficial"),
                     ],
-                )
-            else:
-                me = f"[{ultroid_bot.me.first_name}](tg://user?id={ultroid_bot.uid})"
-                mention = f"[{get_display_name(u)}](tg://user?id={u.id})"
-                await event.reply(
-                    Redis("STARTMSG").format(me=me, mention=mention),
-                    file=udB.get("STARTMEDIA"),
-                    buttons=[
-                        [
-                            Button.url("✵Jᴏin Chᴀnnᴇl✵", url="https://t.me/FutureTechnologyOfficial"),
-                        ],
-                        [
-                            Button.inline("✵Chᴀᴛ wiᴛh CɪᴘʜᴇʀX✵", data="chat"),
-                        ],
-                        [
-                            Button.inline("✵Grᴏuᴩ/Chᴀnnᴇl ʍᴀnᴀgᴇr Hᴇlᴩ✵", data="group"),
-                        ],
-                        [
-                            Button.inline("✵CɪᴘʜᴇʀX Sᴇrvᴇr Ping✵", data="ping"),
-                        ],
-                        [
-                            Button.inline("✵Tᴇxᴛ Trᴀnslᴀᴛᴏr✵", data="tlans"),
-                        ],
-                        [
-                            Button.inline("✵CɪᴘʜᴇʀX Bᴏᴛs Lisᴛ✵", data="list"),
-                        ],
+                    [
+                        Button.inline("✵Chᴀᴛ wiᴛh CɪᴘʜᴇʀX✵", data="chat"),
                     ],
-                )
+                    [
+                        Button.inline("✵Grᴏuᴩ/Chᴀnnᴇl ʍᴀnᴀgᴇr Hᴇlᴩ✵", data="group"),
+                    ],
+                    [
+                        Button.inline("✵CɪᴘʜᴇʀX Sᴇrvᴇr Ping✵", data="ping"),
+                    ],
+                    [
+                        Button.inline("✵Tᴇxᴛ Trᴀnslᴀᴛᴏr✵", data="tlans"),
+                    ],
+                    [
+                        Button.inline("✵CɪᴘʜᴇʀX Bᴏᴛs Lisᴛ✵", data="list"),
+                    ],
+                ],
+            )
         else:
-            name = get_display_name(event.sender_id)
-            if event.pattern_match.group(1) == "set":
-                await event.reply(
-                    "Choose from the below options -",
-                    buttons=_settings,
-                )
-            else:
-                await event.reply(
-                    get_string("ast_3").format(name),
-                    buttons=_start,
-                )
-
-
+            me = f"[{ultroid_bot.me.first_name}](tg://user?id={ultroid_bot.uid})"
+            mention = f"[{get_display_name(u)}](tg://user?id={u.id})"
+            await event.reply(
+                udB.get("STARTMSG").format(me=me, mention=mention),
+                file=udB.get("STARTMEDIA"),
+                buttons=[
+                    [
+                        Button.url("✵Jᴏin Chᴀnnᴇl✵", url="https://t.me/FutureTechnologyOfficial"),
+                    ],
+                    [
+                        Button.inline("✵Chᴀᴛ wiᴛh CɪᴘʜᴇʀX✵", data="chat"),
+                    ],
+                    [
+                        Button.inline("✵Grᴏuᴩ/Chᴀnnᴇl ʍᴀnᴀgᴇr Hᴇlᴩ✵", data="group"),
+                    ],
+                    [
+                        Button.inline("✵CɪᴘʜᴇʀX Sᴇrvᴇr Ping✵", data="ping"),
+                    ],
+                    [
+                        Button.inline("✵Tᴇxᴛ Trᴀnslᴀᴛᴏr✵", data="tlans"),
+                    ],
+                    [
+                        Button.inline("✵CɪᴘʜᴇʀX Bᴏᴛs Lisᴛ✵", data="list"),
+                    ],
+                ],
+            )
+    else:
+        name = get_display_name(event.sender_id)
+        if event.pattern_match.group(1) == "set":
+            await event.reply(
+                "Choose from the below options -",
+                buttons=_settings,
+            )
+        else:
+            await event.reply(
+                get_string("ast_3").format(name),
+                buttons=_start,
+            )
+            
+                
+@callback("itkkstyo", owner=True)
+async def ekekdhdb(e):
+    text = f"When New Visitor will visit your Assistant Bot. You will get this log message!\n\nTo Disable : {HNDLR}setredis OFF_START_LOG True"
+    await e.answer(text, alert=True)
+    
+    
 @callback("tlans")
 async def tlans(event):
     await asst.send_message(event.chat_id, "✨ How to use:\n1⃣ `/tr LangaugeCode text` \n2⃣ `/tr Language Code` as reply to a message \n\nHere is the list of [Language Codes](https://telegra.ph/CɪᴘʜᴇʀX-03-10)", link_preview=False)
 
+    
 @callback("list")
 async def list(event):
     await asst.send_message(event.chat_id, "⚜️ Here is the list of bots made by CɪᴘʜᴇʀX ⚜️\n\n🎆CɪᴘʜᴇʀX Assistant ~ [Bot Link](https://t.me/CipherXBot)\n\n🎆File to Link Generator ~ [Bot Link](https://t.me/FiletoLinkTelegramBot)\n\n🎆Rename with Custom Thumbnail ~ [Bot Link](https://t.me/RenameTelegramBot)\n\n🎆LynX Group Manager ~ [Bot Link](https://t.me/LynXGroupManagerRobot)\n\n🎆Future Technology Chat Bot ~ [Bot Link](https://t.me/FutureTechnologyChatBot)", link_preview=False)
 
+    
 @callback("chat")
 async def chat(event):
     await asst.send_message(event.chat_id, "Send your message please. I'll see and answer you whenever get online\n\n✨ CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ Ⲃⲟⲧ✨")
@@ -153,6 +162,7 @@ async def _(event):
         f"**█▀█ █ █▄░█ █▀▀ █ \n█▀▀ █ █░▀█ █▄█ ▄**\n\n ➲CɪᴘʜᴇʀX Ⲋⲉʀⳳⲉʀ Ⲣⲓⲛⳋ~`{ms}ms`",
     )
 
+    
 @callback("group")
 async def users(event):
     await event.delete()
@@ -160,21 +170,15 @@ async def users(event):
     await asst.send_message(event.chat_id, grabon)
 
 
-
-
-@callback("mainmenu")
-@owner
+@callback("mainmenu", owner=True, func=lambda x: not x.is_group)
 async def ultroid(event):
-    if event.is_group:
-        return
     await event.edit(
         get_string("ast_3").format(OWNER_NAME),
         buttons=_start,
     )
 
-
-@callback("stat")
-@owner
+    
+@callback("stat", owner=True)
 async def botstat(event):
     ok = len(get_all_users())
     msg = """CɪᴘʜᴇʀX Assistant - Stats
@@ -184,44 +188,39 @@ Total Users - {}""".format(
     await event.answer(msg, cache_time=0, alert=True)
 
 
-@callback("bcast")
-@owner
+@callback("bcast", owner=True)
 async def bdcast(event):
     ok = get_all_users()
-    await event.edit(f"Broadcast to {len(ok)} users.")
+    await event.edit(f"• Broadcast to {len(ok)} users.")
     async with event.client.conversation(OWNER_ID) as conv:
         await conv.send_message(
             "Enter your broadcast message.\nUse /cancel to stop the broadcast.",
         )
-        response = conv.wait_event(events.NewMessage(chats=OWNER_ID))
-        response = await response
-        themssg = response.message.message
-        if themssg == "/cancel":
+        response = await conv.get_response()
+        if response.message == "/cancel":
             return await conv.send_message("Cancelled!!")
-        else:
-            success = 0
-            fail = 0
-            await conv.send_message(f"Starting a broadcast to {len(ok)} users...")
-            start = datetime.now()
-            for i in ok:
-                try:
-                    await asst.send_message(int(i), f"{themssg}")
-                    success += 1
-                except BaseException:
-                    fail += 1
-            end = datetime.now()
-            time_taken = (end - start).seconds
-            await conv.send_message(
-                f"""
-Broadcast completed in {time_taken} seconds.
+        success = 0
+        fail = 0
+        await conv.send_message(f"Starting a broadcast to {len(ok)} users...")
+        start = datetime.now()
+        for i in ok:
+            try:
+                await asst.send_message(int(i), response.message)
+                success += 1
+            except BaseException:
+                fail += 1
+        end = datetime.now()
+        time_taken = (end - start).seconds
+        await conv.send_message(
+            f"""
+**Broadcast completed in {time_taken} seconds.**
 Total Users in Bot - {len(ok)}
-Sent to {success} users.
-Failed for {fail} user(s).""",
-            )
+**Sent to** : `{success} users.`
+**Failed for** : `{fail} user(s).`""",
+        )
 
 
-@callback("setter")
-@owner
+@callback("setter", owner=True)
 async def setting(event):
     await event.edit(
         "Choose from the below options -",
@@ -229,8 +228,7 @@ async def setting(event):
     )
 
 
-@callback("tz")
-@owner
+@callback("tz", owner=True)
 async def timezone_(event):
     await event.delete()
     pru = event.sender_id
@@ -248,16 +246,15 @@ async def timezone_(event):
                 "Cancelled!!",
                 buttons=get_back_button("mainmenu"),
             )
-        else:
-            try:
-                tz(themssg)
-                await setit(event, var, themssg)
-                await conv.send_message(
-                    f"{name} changed to {themssg}\n",
-                    buttons=get_back_button("mainmenu"),
-                )
-            except BaseException:
-                await conv.send_message(
-                    "Wrong TimeZone, Try again",
-                    buttons=get_back_button("mainmenu"),
-                )
+        try:
+            tz(themssg)
+            await setit(event, var, themssg)
+            await conv.send_message(
+                f"{name} changed to {themssg}\n",
+                buttons=get_back_button("mainmenu"),
+            )
+        except BaseException:
+            await conv.send_message(
+                "Wrong TimeZone, Try again",
+                buttons=get_back_button("mainmenu"),
+            )
