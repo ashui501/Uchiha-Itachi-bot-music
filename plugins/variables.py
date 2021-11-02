fda6b5af2639ef7ba05ccfea83f8849b90c577e9
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-02 21:57:38 +0330

    Update variables.py

--- plugins/variables.py
+++ plugins/variables.py
@@ -16,45 +16,40 @@
 
 import os
 
-from . import *
+from . import eor, get_string, udB, ultroid_cmd
 
 
-@ultroid_cmd(pattern="get")
+@ultroid_cmd(pattern="get", fullsudo=True)
 async def get_var(event):
-    if not event.out and not is_fullsudo(event.sender_id):
-        return await eod(event, "`This Command Is Sudo Restricted.`")
-    if len(event.text) > 4:
-        if " " in event.text[4]:
-            opt = event.text.split(" ", maxsplit=2)[1]
-        else:
-            return
+    if len(event.text) > 4 and " " in event.text[4]:
+        opt = event.text.split(" ", maxsplit=2)[1]
     else:
         return
     x = await eor(event, get_string("com_1"))
-    if not opt == "keys":
+    if opt != "keys":
         try:
             varname = event.text.split(" ", maxsplit=2)[2]
         except IndexError:
-            return await eod(x, "Such a var doesn't exist!", time=5)
+            return await eor(x, "Such a var doesn't exist!", time=5)
     if opt == "var":
         c = 0
         # try redis
         val = udB.get(varname)
         if val is not None:
             c += 1
-            return await x.edit(
+            await x.edit(
                 f"**Ⳳⲁʀⲓⲁⲃⳑⲉ** - `{varname}`\n**Ⳳⲁⳑυⲉ**: `{val}`\n**Ⲧⲩⲣⲉ**: Rⲉⲇⲓⲋ Ⲕⲉⲩ."
             )
         # try env vars
         val = os.getenv(varname)
         if val is not None:
             c += 1
-            return await x.edit(
+            await x.edit(
                 f"**Ⳳⲁʀⲓⲁⲃⳑⲉ** - `{varname}`\n**Ⳳⲁⳑυⲉ**: `{val}`\n**Ⲧⲩⲣⲉ**: Ⲉⲛⳳ Ⳳⲁʀ."
             )
 
         if c == 0:
-            return await eod(x, "Such a var doesn't exist!", time=5)
+            await eor(x, "Such a var doesn't exist!", time=5)
 
     elif opt == "type":
         c = 0
@@ -62,32 +57,31 @@ async def get_var(event):
         val = udB.get(varname)
         if val is not None:
             c += 1
-            return await x.edit(f"**Ⳳⲁʀⲓⲁⲃⳑⲉ** - `{varname}`\n**Ⲧⲩⲣⲉ**: Rⲉⲇⲓⲋ Ⲕⲉⲩ.")
+            await x.edit(f"**Ⳳⲁʀⲓⲁⲃⳑⲉ** - `{varname}`\n**Ⲧⲩⲣⲉ**: Rⲉⲇⲓⲋ Ⲕⲉⲩ.")
         # try env vars
         val = os.getenv(varname)
         if val is not None:
             c += 1
-            return await x.edit(f"**Ⳳⲁʀⲓⲁⲃⳑⲉ** - `{varname}`\n**Ⲧⲩⲣⲉ**: Ⲉⲛⳳ Ⳳⲁʀ.")
+            await x.edit(f"**Ⳳⲁʀⲓⲁⲃⳑⲉ** - `{varname}`\n**Ⲧⲩⲣⲉ**: Ⲉⲛⳳ Ⳳⲁʀ.")
 
         if c == 0:
-            return await eod(x, "Such a var doesn't exist!", time=5)
+            await eor(x, "Such a var doesn't exist!", time=5)
 
     elif opt == "redis":
         val = udB.get(varname)
         if val is not None:
-            return await x.edit(f"**Ⲕⲉⲩ** - `{varname}`\n**Ⳳⲁⳑυⲉ**: `{val}`")
+            await x.edit(f"**Ⲕⲉⲩ** - `{varname}`\n**Ⳳⲁⳑυⲉ**: `{val}`")
         else:
-            return await eod(x, "No such key!", time=5)
+            await eor(x, "No such key!", time=5)
 
     elif opt == "keys":
         keys = sorted(udB.keys())
-        msg = ""
-        for i in keys:
-            if i.isdigit() or i.startswith("-") or i.startswith("GBAN_REASON_"):
-                pass
-            else:
-                msg += f"• `{i}`" + "\n"
+        msg = "".join(
+            f"• `{i}`" + "\n"
+            for i in keys
+            if not i.isdigit()
+            and not i.startswith("-")
+            and not i.startswith("GBAN_REASON_")
+        )
+
         await x.edit(f"**Ⳑⲓⲋⲧ ⲟϝ Rⲉⲇⲓⲋ Ⲕⲉⲩⲋ :**\n{msg}")
-
-
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
