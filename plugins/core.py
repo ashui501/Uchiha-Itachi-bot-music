08ccbb6cc345321aec12de053c2c1a24adb3ecc1
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-02 19:06:09 +0330

    Update core.py

--- plugins/core.py
+++ plugins/core.py
@@ -1,16 +1,9 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
 • `{i}install <reply to plugin>`
     To install the plugin,
-   `{i}install f`
+  `{i}install f`
     To force Install.
 
 • `{i}uninstall <plugin name>`
@@ -24,21 +17,20 @@
 
 • `{i}help <plugin name>`
     Shows you a help menu (like this) for every plugin.
+
+• `{i}getaddons <raw link to code>`
+    Load Plugins from the given raw link.
 """
 
 import os
 
-from telethon import Button
+from cython.startup.loader import load_addons
 
-from . import *
+from . import eod, eor, get_string, requests, safeinstall, ultroid_cmd, un_plug
 
 
-@ultroid_cmd(
-    pattern="install",
-)
+@ultroid_cmd(pattern="install", fullsudo=True)
 async def install(event):
-    if not event.out and not is_fullsudo(event.sender_id):
-            return await eor(event, "`This Command is Sudo Restricted.`")
     await safeinstall(event)
 
 
@@ -46,12 +38,9 @@ async def install(event):
     pattern=r"unload ?(.*)",
 )
 async def unload(event):
-    if not event.out:
-        if not is_fullsudo(event.sender_id):
-            return await eor(event, "`This Command is Sudo Restricted.`")
     shortname = event.pattern_match.group(1)
     if not shortname:
-        await eor(event, "`Give name of plugin which u want to unload`")
+        await eor(event, get_string("core_9"))
         return
     lsd = os.listdir("addons")
     lst = os.listdir("plugins")
@@ -59,25 +48,22 @@ async def unload(event):
     if zym in lsd:
         try:
             un_plug(shortname)
-            await eod(event, f"**Uɴʟᴏᴀᴅᴇᴅ** `{shortname}` **Sᴜᴄᴄᴇssғᴜʟʟʏ.**", time=3)
+            await eor(event, f"**Uɴʟᴏᴀᴅᴇᴅ** `{shortname}` **Sᴜᴄᴄᴇssғᴜʟʟʏ.**", time=3)
         except Exception as ex:
             return await eor(event, str(ex))
     elif zym in lst:
-        return await eod(event, "**Yᴏᴜ Cᴀɴ'ᴛ Uɴʟᴏᴀᴅ Oғғɪᴄɪᴀʟ Pʟᴜɢɪɴs**", time=3)
+        return await eor(event, get_string("core_11"), time=3)
     else:
-        return await eod(event, f"**Nᴏ Pʟᴜɢɪɴ Nᴀᴍᴇᴅ** `{shortname}`", time=3)
+        return await eor(event, f"**Nᴏ Pʟᴜɢɪɴ Nᴀᴍᴇᴅ** `{shortname}`", time=3)
 
 
 @ultroid_cmd(
     pattern=r"uninstall ?(.*)",
 )
 async def uninstall(event):
-    if not event.out:
-        if not is_fullsudo(event.sender_id):
-            return await eor(event, "`This Command is Sudo Restricted.`")
     shortname = event.pattern_match.group(1)
     if not shortname:
-        await eor(event, "`Give name of plugin which you want to uninstall`")
+        await eor(event, get_string("core_13"))
         return
     lsd = os.listdir("addons")
     lst = os.listdir("plugins")
@@ -85,26 +71,24 @@ async def uninstall(event):
     if zym in lsd:
         try:
             un_plug(shortname)
-            await eod(event, f"**Uɴɪɴsᴛᴀʟʟᴇᴅ** `{shortname}` **Sᴜᴄᴄᴇssғᴜʟʟʏ.**", time=3)
+            await eor(event, f"**Uɴɪɴsᴛᴀʟʟᴇᴅ** `{shortname}` **Sᴜᴄᴄᴇssғᴜʟʟʏ.**", time=3)
             os.remove(f"addons/{shortname}.py")
         except Exception as ex:
             return await eor(event, str(ex))
     elif zym in lst:
-        return await eod(event, "**Yᴏᴜ Cᴀɴ'ᴛ Uɴɪɴsᴛᴀʟʟ Oғғɪᴄɪᴀʟ Pʟᴜɢɪɴs**", time=3)
+        return await eor(event, get_string("core_15"), time=3)
     else:
-        return await eod(event, f"**Nᴏ Pʟᴜɢɪɴ Nᴀᴍᴇᴅ** `{shortname}`", time=3)
+        return await eor(event, f"**Nᴏ Pʟᴜɢɪɴ Nᴀᴍᴇᴅ** `{shortname}`", time=3)
 
 
 @ultroid_cmd(
     pattern=r"load ?(.*)",
+    fullsudo=True,
 )
 async def load(event):
-    if not event.out:
-        if not is_fullsudo(event.sender_id):
-            return await eor(event, "`This Command is Sudo Restricted.`")
     shortname = event.pattern_match.group(1)
     if not shortname:
-        await eor(event, "`Give name of plugin which you want to load`")
+        await eor(event, get_string("core_16"))
         return
     try:
         try:
@@ -112,10 +96,39 @@ async def load(event):
         except BaseException:
             pass
         load_addons(shortname)
-        await eod(event, f"**Sᴜᴄᴄᴇssғᴜʟʟʏ Lᴏᴀᴅᴇᴅ** `{shortname}`", time=3)
+        await eor(event, get_string("core_17").format(shortname), time=3)
     except Exception as e:
         await eod(
             event,
-            f"**Could not load** `{shortname}` **because of the following error.**\n`{str(e)}`",
+            get_string("core_18").format(shortname, e),
+            time=3,
+        )
+
+
+@ultroid_cmd(pattern="getaddons ?(.*)", fullsudo=True)
+async def get_the_addons_lol(event):
+    thelink = event.pattern_match.group(1)
+    xx = await eor(event, get_string("com_1"))
+    fool = get_string("gas_1")
+    if thelink is None:
+        return await eor(xx, fool, time=10)
+    split_thelink = thelink.split("/")
+    if "raw" not in thelink:
+        return await eor(xx, fool, time=10)
+    name_of_it = split_thelink[(len(split_thelink) - 1)]
+    plug = requests.get(thelink).text
+    fil = f"addons/{name_of_it}"
+    await xx.edit("Packing the codes...")
+    with open(fil, "w", encoding="utf-8") as uult:
+        uult.write(plug)
+    await xx.edit("Packed. Now loading the plugin..")
+    shortname = name_of_it.split(".")[0]
+    try:
+        load_addons(shortname)
+        await eor(xx, get_string("core_17").format(shortname), time=15)
+    except Exception as e:
+        await eod(
+            xx,
+            get_string("core_18").format(shortname, e),
             time=3,
         )
