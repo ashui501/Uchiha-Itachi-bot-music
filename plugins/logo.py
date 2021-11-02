f91d40cc356d36bbabf34eef9f5e1e959a3d7027
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-02 19:44:36 +0330

    Update logo.py

--- plugins/logo.py
+++ plugins/logo.py
@@ -1,37 +1,38 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
 • `{i}logo <text>`
    Generate a logo of the given Text
-   Or Reply To image , to write your text on it.
+   Or Reply To image , to write ur text on it.
    Or Reply To Font File, To write with that font.
 
 """
-
 import glob
 import os
 import random
 
 from PIL import Image, ImageDraw, ImageFont
+from cython.functions.misc import unsplashsearch
 from telethon.tl.types import InputMessagesFilterPhotos
 
-from . import *
+from . import (
+    OWNER_ID,
+    OWNER_NAME,
+    download_file,
+    eor,
+    get_string,
+    mediainfo,
+    ultroid_cmd,
+)
 
 
 @ultroid_cmd(pattern="logo ?(.*)")
 async def logo_gen(event):
-    xx = await eor(event, "`...`")
+    xx = await eor(event, get_string("com_1"))
     name = event.pattern_match.group(1)
     if not name:
-        await eod(xx, "`Give a name too!`")
-    bg_, font_ = "", ""
+        await eor(xx, "`Give a name too!`", time=5)
+    bg_, font_ = None, None
     if event.reply_to_msg_id:
         temp = await event.get_reply_message()
         if temp.media:
@@ -42,24 +43,20 @@ async def logo_gen(event):
                     font_ = await temp.download_media()
             elif "pic" in mediainfo(temp.media):
                 bg_ = await temp.download_media()
-    else:
-        pics = []
-        async for i in event.client.iter_messages(
-            "@UltroidLogos", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
-        fpath_ = glob.glob("resources/fonts/*")
-        font_ = random.choice(fpath_)
     if not bg_:
-        pics = []
-        async for i in event.client.iter_messages(
-            "@UltroidLogos", filter=InputMessagesFilterPhotos
-        ):
-            pics.append(i)
-        id_ = random.choice(pics)
-        bg_ = await id_.download_media()
+        if event.client._bot:
+            SRCH = ["blur background", "background", "neon lights", "wallpaper"]
+            res = await unsplashsearch(random.choice(SRCH), limit=1)
+            bg_ = await download_file(res[0], "resources/downloads/logo.png")
+        else:
+            pics = []
+            async for i in event.client.iter_messages(
+                "@UltroidLogos", filter=InputMessagesFilterPhotos
+            ):
+                pics.append(i)
+            id_ = random.choice(pics)
+            bg_ = await id_.download_media()
+
     if not font_:
         fpath_ = glob.glob("resources/fonts/*")
         font_ = random.choice(fpath_)
@@ -89,7 +86,7 @@ async def logo_gen(event):
     draw.text(
         (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
     )
-    flnme = f"cipherx.png"
+    flnme = "cipherx.png"
     img.save(flnme, "png")
     await xx.edit("`☑️`")
     await xx.edit("`✅`")
@@ -97,7 +94,6 @@ async def logo_gen(event):
         await event.client.send_file(
             event.chat_id,
             file=flnme,
-            reply_to=event.reply_to_msg_id, 
             caption=f"𑀉᧐g᧐ ⲙᥲɗᥱ δy CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ Ⲃⲟⲧ",
             force_document=False,
         )
@@ -105,6 +101,5 @@ async def logo_gen(event):
         await xx.delete()
     if os.path.exists(bg_):
         os.remove(bg_)
-    if os.path.exists(font_):
-        if not font_.startswith("resources/fonts"):
-            os.remove(font_)
+    if os.path.exists(font_) and not font_.startswith("resources/fonts"):
+        os.remove(font_)
