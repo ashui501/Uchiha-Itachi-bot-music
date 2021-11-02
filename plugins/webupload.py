5b1a53f8cf06f873b0671f633438565936515586
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-02 22:07:21 +0330

    Update webupload.py

--- plugins/webupload.py
+++ plugins/webupload.py
@@ -1,10 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
@@ -17,51 +10,50 @@ import time
 from telethon.errors.rpcerrorlist import BotInlineDisabledError as dis
 from telethon.errors.rpcerrorlist import BotResponseTimeoutError as rep
 
-from . import *
+from . import HNDLR, asst, downloader, eor, get_string, ultroid_cmd
 
 
 @ultroid_cmd(
     pattern="webupload",
 )
 async def _(event):
-    xx = await eor(event, "`Processing...`")
+    xx = await eor(event, get_string("com_1"))
     vv = event.text.split(" ", maxsplit=1)
     try:
         file_name = vv[1]
     except IndexError:
-        pass
-    if event.reply_to_msg_id:
-        bb = await event.get_reply_message()
-        if bb.media:
-            ccc = time.time()
-            try:
-                naam = await downloader(
-                    bb.file.name,
-                    bb.media.document,
-                    xx,
-                    ccc,
-                    "Downloading " + bb.file.name + "...",
-                )
-                file_name = naam.name
-            except BaseException:
-                file_name = await event.client.download_media(bb)
-        else:
-            return await eod(xx, "`Reply to media file`", time=5)
+        return await eor(xx, get_string("wbl_1"))
+    bb = await event.get_reply_message()
+    if not (bb and bb.media):
+        return await eor(xx, get_string("cvt_3"))
+    ccc = time.time()
+    try:
+        naam = await downloader(
+            bb.file.name,
+            bb.media.document,
+            xx,
+            ccc,
+            "Downloading " + bb.file.name + "...",
+        )
+        file_name = naam.name
+    except BaseException:
+        file_name = await event.client.download_media(bb)
     try:
         results = await event.client.inline_query(
             asst.me.username,
-            f"fl2lnk {file_name}"
+            f"fl2lnk {file_name}",
         )
     except rep:
         return await eor(
             xx,
-            "`The bot did not respond to the inline query.\nConsider using {}restart`".format(
+            get_string("help_2").format(
                 HNDLR,
             ),
         )
     except dis:
         return await eor(
-            xx, "`Please turn on inline mode for your bot from` @Botfather.",
+            xx,
+            get_string("help_3"),
         )
     await results[0].click(event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True)
     await xx.delete()
