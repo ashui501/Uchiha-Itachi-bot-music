32225a87ff9c6a26b94e2f83c337fa634d0abc02
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-02 19:19:09 +0330

    Update filter.py

--- plugins/filter.py
+++ plugins/filter.py
@@ -1,10 +1,3 @@
-# Ultroid - UserBot
-# Copyright (C) 2021 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
@@ -21,12 +14,14 @@
 import os
 import re
 
-from cython.functions.filter_db import *
+from cython.dB.filter_db import add_filter, get_filter, list_filter, rem_filter
+from cython.functions.tools import create_tl_btn, format_btn, get_msg_button
 from telegraph import upload_file as uf
 from telethon.tl.types import User
 from telethon.utils import pack_bot_file_id
 
-from . import *
+from . import eor, events, get_string, mediainfo, ultroid_bot, ultroid_cmd
+from ._inline import something
 
 
 @ultroid_cmd(pattern="addfilter ?(.*)")
@@ -35,7 +30,8 @@ async def af(e):
     wt = await e.get_reply_message()
     chat = e.chat_id
     if not (wt and wrd):
-        return await eor(e, "`Use this command word to set as filter and reply...`")
+        return await eor(e, get_string("flr_1"))
+    btn = format_btn(wt.buttons) if wt.buttons else None
     if wt and wt.media:
         wut = mediainfo(wt.media)
         if wut.startswith(("pic", "gif")):
@@ -44,21 +40,26 @@ async def af(e):
             m = "https://telegra.ph" + variable[0]
         elif wut == "video":
             if wt.media.document.size > 8 * 1000 * 1000:
-                return await eod(x, "`Unsupported Media`")
-            else:
-                dl = await wt.download_media()
-                variable = uf(dl)
-                os.remove(dl)
-                m = "https://telegra.ph" + variable[0]
+                return await eor(e, get_string("com_4"), time=5)
+            dl = await wt.download_media()
+            variable = uf(dl)
+            os.remove(dl)
+            m = "https://telegra.ph" + variable[0]
         else:
             m = pack_bot_file_id(wt.media)
         if wt.text:
-            add_filter(int(chat), wrd, wt.text, m)
+            txt = wt.text
+            if not btn:
+                txt, btn = get_msg_button(wt.text)
+            add_filter(int(chat), wrd, txt, m, btn)
         else:
-            add_filter(int(chat), wrd, None, m)
+            add_filter(int(chat), wrd, None, m, btn)
     else:
-        add_filter(int(chat), wrd, wt.text, None)
-    await eor(e, f"Done : Filter `{wrd}` Saved.")
+        txt = wt.text
+        if not btn:
+            txt, btn = get_msg_button(wt.text)
+        add_filter(int(chat), wrd, txt, None, btn)
+    await eor(e, get_string("flr_4").format(wrd))
 
 
 @ultroid_cmd(pattern="remfilter ?(.*)")
@@ -66,9 +67,9 @@ async def rf(e):
     wrd = (e.pattern_match.group(1)).lower()
     chat = e.chat_id
     if not wrd:
-        return await eor(e, "`Give the filter to remove...`")
+        return await eor(e, get_string("flr_3"))
     rem_filter(int(chat), wrd)
-    await eor(e, f"Done : Filter `{wrd}` Removed.")
+    await eor(e, get_string("flr_5").format(wrd))
 
 
 @ultroid_cmd(pattern="listfilter$")
@@ -78,7 +79,7 @@ async def lsnote(e):
         sd = "Filters Found In This Chats Are\n\n"
         await eor(e, sd + x)
     else:
-        await eor(e, "No Filters Found Here")
+        await eor(e, get_string("flr_6"))
 
 
 @ultroid_bot.on(events.NewMessage())
@@ -87,13 +88,16 @@ async def fl(e):
         return
     xx = (e.text).lower()
     chat = e.chat_id
-    x = get_filter(int(chat))
+    x = get_filter(chat)
     if x:
         for c in x:
             pat = r"( |^|[^\w])" + re.escape(c) + r"( |$|[^\w])"
             if re.search(pat, xx):
-                k = get_reply(int(chat), c)
+                k = x.get(c)
                 if k:
                     msg = k["msg"]
                     media = k["media"]
+                    if k.get("button"):
+                        btn = create_tl_btn(k["button"])
+                        return await something(e, msg, media, btn)
                     await e.reply(msg, file=media)
