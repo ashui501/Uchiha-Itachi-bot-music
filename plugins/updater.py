27ac16147f44ea7e9214db55a8ef898cdc95d343
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-02 21:22:11 +0330

    Update updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -1,32 +1,48 @@
-# Ultroid - UserBot
-# Copyright (C) 2021 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
+
 • `{i}update`
     See changelogs if any update is available.
 """
 
+from random import choice
+
 from git import Repo
-from telethon.tl.functions.channels import ExportMessageLinkRequest as GetLink
+from cython.dB import ULTROID_IMAGES
 
-from . import *
+from . import (
+    INLINE_PIC,
+    Button,
+    asst,
+    bash,
+    call_back,
+    callback,
+    eor,
+    eod,
+    get_string,
+    os,
+    sys,
+    udB,
+    ultroid_cmd,
+    updater,
+)
 
-ULTPIC = "resources/extras/cipherx.jpg"
-CL = udB.get("INLINE_PIC")
-if CL:
-    ULTPIC = CL
+ULTPIC = INLINE_PIC or choice(ULTROID_IMAGES)
 
 
-@ultroid_cmd(pattern="update$")
+@ultroid_cmd(pattern="update ?(.*)")
 async def _(e):
-    m = await updater()
+    m = updater()
     branch = (Repo.init()).active_branch
     if m:
+        if e.pattern_match.group(1) and (
+            "fast" in e.pattern_match.group(1) or "soft" in e.pattern_match.group(1)
+        ):
+            await bash("git pull -f && pip3 install -r requirements.txt")
+            call_back()
+            await xx.edit(get_string("upd_7"))
+            os.execl(sys.executable, "python3", "-m", "cython")
+            return
         x = await asst.send_file(
             int(udB.get("LOG_CHANNEL")),
             ULTPIC,
@@ -34,10 +50,7 @@ async def _(e):
             force_document=False,
             buttons=Button.inline("Changelogs", data="changes"),
         )
-        if not e.client._bot:
-            Link = (await e.client(GetLink(x.chat_id, x.id))).link
-        else:
-            Link = f"https://t.me/c/{x.chat.id}/{x.id}"
+        Link = x.message_link
         await eod(
             e,
             f'<strong><a href="{Link}">[ChangeLogs]</a></strong>',
@@ -48,15 +61,14 @@ async def _(e):
     else:
         await eod(
             e,
-            f'<code>CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ : \n</code><strong>｡⌬｡ up-to-date ｡⌬｡</strong>',
+            f'<code>CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ Ⲃⲟⲧ : \n</code><strong>｡⌬｡ up-to-date ｡⌬｡</strong>',
             parse_mode="html",
-            time=10, 
+            time=3,
             link_preview=False,
         )
 
 
-@callback("updtavail")
-@owner
+@callback("updtavail", owner=True)
 async def updava(event):
     await event.delete()
     await asst.send_file(
