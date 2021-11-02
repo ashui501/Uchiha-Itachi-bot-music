3cf596180dcf6947072b7275eac52ae2392815ab
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-02 22:05:55 +0330

    Update webshot.py

--- plugins/webshot.py
+++ plugins/webshot.py
@@ -1,67 +1,32 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
+
 • `{i}webshot <url>`
     Get a screenshot of the webpage.
+
 """
-import io
-import requests
-from selenium import webdriver
+import os
 
-from . import *
+from htmlwebshot import WebShot
+
+from . import eor, get_string, is_url_ok, ultroid_cmd
 
 
-@ultroid_cmd(pattern="webshot")
+@ultroid_cmd(pattern="webshot ?(.*)")
 async def webss(event):
     xx = await eor(event, get_string("com_1"))
-    mssg = event.text.split(" ", maxsplit=2)
-    try:
-        xurl = mssg[1]
-    except IndexError:
-        return await eod(xx, "`Give a URL please!`", time=5)
-    try:
-        requests.get(xurl)
-    except requests.ConnectionError:
-        return await eod(xx, "Invalid URL!", time=5)
-    except requests.exceptions.MissingSchema:
-        try:
-            r = requests.get("https://" + xurl)
-        except requests.ConnectionError:
-            try:
-                r2 = requests.get("http://" + xurl)
-            except requests.ConnectionError:
-                return await eod(xx, "Invalid URL!", time=5)
-
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--ignore-certificate-errors")
-    chrome_options.add_argument("--test-type")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.binary_location = "/usr/bin/google-chrome"
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get(xurl)
-    height = driver.execute_script(
-        "return Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);"
+    xurl = event.pattern_match.group(1)
+    if not xurl:
+        return await eor(xx, get_string("wbs_1"), time=5)
+    elif not is_url_ok(xurl):
+        return await eor(xx, get_string("wbs_2"), time=5)
+    shot = WebShot(quality=88, flags=["--enable-javascript", "--no-stop-slow-scripts"])
+    pic = await shot.create_pic_async(url=xurl)
+    await xx.reply(
+        get_string("wbs_3").format(xurl),
+        file=pic,
+        link_preview=False,
+        force_document=True,
     )
-    driver.set_window_size(width + 100, height + 100)
-    width = driver.execute_script(
-        "return Math.max(document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth);"
-    )
-    im_png = driver.get_screenshot_as_png()
-    driver.close()
-    with io.BytesIO(im_png) as sshot:
-        sshot.name = "webshot.png"
-        await xx.reply(
-            f"**WebShot Generated**\n**URL**: {xurl}",
-            file=sshot,
-            link_preview=False,
-            force_document=True,
-        )
+    os.remove(pic)
     await xx.delete()
