845187c8f3cfb0224749055ee91ce1f162c67bc5
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-02 19:10:55 +0330

    Update devtools.py

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -1,17 +1,10 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
 """
 ✘ Commands Available -
 
 • `{i}bash <cmds>`
     Run linux commands on telegram.
 
-• `{i}eval <cmds>`
+• `{i}eval <code>`
     Evaluate python commands on telegram.
     Shortcuts:
         client = bot = event.client
@@ -19,6 +12,9 @@
         p = print
         reply = await event.get_reply_message()
         chat = event.chat_id
+
+• `{i}cpp <code>`
+    Run c++ code from Telegram.
 
 • `{i}sysinfo`
     Shows System Info.
@@ -28,7 +24,11 @@ import sys
 import traceback
 from os import remove
 from pprint import pprint
-from carbonnow import Carbon
+
+try:
+    from carbonnow import Carbon
+except ImportError:
+    Carbon = None
 
 from . import *
 
@@ -37,36 +37,24 @@ from . import *
     pattern="sysinfo$",
 )
 async def _(e):
-    xx = await eor(e, "`Sending...`")
+    xx = await eor(e, get_string("com_1"))
     x, y = await bash("neofetch|sed 's/\x1B\\[[0-9;\\?]*[a-zA-Z]//g' >> neo.txt")
     with open("neo.txt", "r") as neo:
         p = (neo.read()).replace("\n\n", "")
     ok = Carbon(base_url="https://carbonara.vercel.app/api/cook", code=p)
-    haa = await ok.save("neofetch")
-    reply = await e.get_reply_message()
+    haa = await ok.memorize("neofetch")
     await e.reply(file=haa)
     await xx.delete()
-    remove("neofetch.jpg")
     remove("neo.txt")
 
 
-@ultroid_cmd(
-    pattern="bash",
-)
+@ultroid_cmd(pattern="bash", fullsudo=True, only_devs=True)
 async def _(event):
-    if not event.out and not is_fullsudo(event.sender_id):
-        return await eor(event, "`This Command is Sudo Restricted.`")
-    if Redis("I_DEV") != "True":
-        await eor(
-            event,
-            f"Developer Restricted!\nIf you know what this does, and want to proceed\n\n `{HNDLR}setredis I_DEV True`\n\nThis Might Be Dangerous.",
-        )
-        return
-    xx = await eor(event, "`Processing...`")
+    xx = await eor(event, get_string("com_1"))
     try:
         cmd = event.text.split(" ", maxsplit=1)[1]
     except IndexError:
-        return await eod(xx, "`No cmd given...`", time=10)
+        return await eor(xx, get_string("devs_1"), time=10)
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
@@ -79,9 +67,9 @@ async def _(event):
         o = "\n".join(_o)
         OUT += f"**• Ⲟυⲧⲣυⲧ:**\n`{o}`"
     if not stderr and not stdout:
-        OUT += f"**• Ⲟυⲧⲣυⲧ:**\n`Ⲋυⲥⲥⲉⲋⲋ`"
+        OUT += "**• Ⲟυⲧⲣυⲧ:**\n`Ⲋυⲥⲥⲉⲋⲋ`"
     if len(OUT) > 4096:
-        ultd = OUT.replace("`", "").replace("*", "").replace("_", "")
+        ultd = OUT.replace("`", "").replace("**", "").replace("__", "")
         with io.BytesIO(str.encode(ultd)) as out_file:
             out_file.name = "bash.txt"
             await event.client.send_file(
@@ -90,37 +78,28 @@ async def _(event):
                 force_document=True,
                 thumb="resources/extras/cipherx.jpg",
                 allow_cache=False,
-                caption=f"`{cmd}`",
+                caption=f"`{cmd}`" if len(cmd) < 998 else None,
                 reply_to=reply_to_id,
             )
+
             await xx.delete()
     else:
         await xx.edit(OUT)
 
 
 p, pp = print, pprint  # ignore: pylint
+bot = ultroid = ultroid_bot
 
 
-@ultroid_cmd(
-    pattern="eval",
-)
+@ultroid_cmd(pattern="eval", fullsudo=True, only_devs=True)
 async def _(event):
-    if len(event.text) > 5:
-        if not event.text[5] == " ":
-            return
-    if not event.out and not is_fullsudo(event.sender_id):
-        return await eor(event, "`This Command is Sudo Restricted.`")
-    if Redis("I_DEV") != "True":
-        await eor(
-            event,
-            f"Developer Restricted!\nIf you know what this does, and want to proceed\n\n {HNDLR}setredis I_DEV True\n\nThis Might Be Dangerous.",
-        )
+    if len(event.text) > 5 and event.text[5] != " ":
         return
-    xx = await eor(event, "`Processing ...`")
+    xx = await eor(event, get_string("com_1"))
     try:
         cmd = event.text.split(" ", maxsplit=1)[1]
     except IndexError:
-        return await eod(xx, "`Give some python cmd...`", time=5)
+        return await eor(xx, get_string("devs_2"), time=5)
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     old_stderr = sys.stderr
@@ -145,7 +124,7 @@ async def _(event):
     elif stdout:
         evaluation = stdout
     else:
-        evaluation = "Ⲋυⲥⲥⲉⲋⲋ"
+        evaluation = get_string("instu_4")
     final_output = (
         "__►__ **Ⲉⳳⲁⳑ**\n```{}``` \n\n __►__ **Ⲟυⲧⲣυⲧ**: \n```{}``` \n".format(
             cmd,
@@ -153,7 +132,7 @@ async def _(event):
         )
     )
     if len(final_output) > 4096:
-        ultd = final_output.replace("`", "").replace("*", "").replace("_", "")
+        ultd = final_output.replace("`", "").replace("**", "").replace("__", "")
         with io.BytesIO(str.encode(ultd)) as out_file:
             out_file.name = "eval.txt"
             await event.client.send_file(
@@ -162,21 +141,72 @@ async def _(event):
                 force_document=True,
                 thumb="resources/extras/cipherx.jpg",
                 allow_cache=False,
-                caption=f"`{cmd}`" if (len(cmd) + 2) < 1000 else None,
+                caption=f"```{cmd}```" if len(cmd) < 998 else None,
                 reply_to=reply_to_id,
             )
             await xx.delete()
     else:
-        await eor(xx, final_output)
+        await xx.edit(final_output)
 
 
 async def aexec(code, event):
     exec(
-        f"async def __aexec(e, client): "
-        + "\n message = event = e"
-        + "\n reply = await event.get_reply_message()"
-        + "\n chat = (await event.get_chat()).id"
-        + "".join(f"\n {l}" for l in code.split("\n")),
+        (
+            (
+                ("async def __aexec(e, client): " + "\n message = event = e")
+                + "\n reply = await event.get_reply_message()"
+            )
+            + "\n chat = (await event.get_chat()).id"
+        )
+        + "".join(f"\n {l}" for l in code.split("\n"))
     )
 
     return await locals()["__aexec"](event, event.client)
+
+
+DUMMY_CPP = """#include <iostream>
+using namespace std;
+
+int main(){
+!code
+}
+"""
+
+
+@ultroid_cmd(pattern="cpp", only_devs=True)
+async def doie(e):
+    match = e.text.split(" ", maxsplit=1)
+    try:
+        match = match[1]
+    except IndexError:
+        return await eor(e, get_string("devs_3"))
+    msg = await eor(e, get_string("com_1"))
+    if "main(" not in match:
+        new_m = "".join(" " * 4 + i + "\n" for i in match.split("\n"))
+        match = DUMMY_CPP.replace("!code", new_m)
+    open("cpp-cipherx.cpp", "w").write(match)
+    m = await bash("g++ -o CppCipherX cpp-cipherx.cpp")
+    o_cpp = f"• **Eval-Cpp**\n`{match}`"
+    if m[1] != "":
+        o_cpp += f"\n\n**• Error :**\n`{m[1]}`"
+        if len(o_cpp) > 3000:
+            os.remove("cpp-cipherx.cpp")
+            if os.path.exists("CppCipherX"):
+                os.remove("CppCipherX")
+            with io.BytesIO(str.encode(o_cpp)) as out_file:
+                out_file.name = "error.txt"
+                return await msg.reply(f"`{match}`", file=out_file)
+        return await eor(msg, o_cpp)
+    m = await bash("./CppCipherX")
+    if m[0] != "":
+        o_cpp += f"\n\n**• Output :**\n`{m[0]}`"
+    if m[1] != "":
+        o_cpp += f"\n\n**• Error :**\n`{m[1]}`"
+    if len(o_cpp) > 3000:
+        with io.BytesIO(str.encode(o_cpp)) as out_file:
+            out_file.name = "eval.txt"
+            await msg.reply(f"`{match}`", file=out_file)
+    else:
+        await eor(msg, o_cpp)
+    os.remove("CppCipherX")
+    os.remove("cpp-cipherx.cpp")
