c1cf9555f9f694ea1c32ea845ceb98653ae141eb
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-11-04 10:29:06 +0330

    Create _cipherx.py

---
+++ plugins/_cipherx.py
@@ -0,0 +1,54 @@
+from telethon.errors import (
+    BotMethodInvalidError,
+    ChatSendInlineForbiddenError,
+    ChatSendMediaForbiddenError,
+)
+
+from . import LOG_CHANNEL, LOGS, Button, asst, eor, get_string, ultroid_cmd
+
+REPOMSG = """
+• **CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ Ⲃⲟⲧ** •\n
+• Repo - [Click Here](https://xhamsterlive.com)
+• Support - @FutureTechnologyOfficial
+"""
+
+RP_BUTTONS = [
+    [Button.url(get_string("bot_3"), "https://xhamsterlive.com")],
+    [Button.url("Support Channel", "t.me/FutureTechnologyOfficial")],
+]
+
+ULTSTRING = """🎇 **Thanks for Deploying CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ Ⲃⲟⲧ**
+
+• Here, are the Some Basic stuff from, where you can Know, about its Usage."""
+
+
+@ultroid_cmd(
+    pattern="repo$",
+    type=["official", "manager"],
+)
+async def repify(e):
+    try:
+        q = await e.client.inline_query(asst.me.username, "")
+        await q[0].click(e.chat_id)
+        return await e.delete()
+    except (
+        ChatSendInlineForbiddenError,
+        ChatSendMediaForbiddenError,
+        BotMethodInvalidError,
+    ):
+        pass
+    except Exception as er:
+        LOGS.info("Error while repo command : " + str(er))
+    await eor(e, REPOMSG)
+
+
+@ultroid_cmd(pattern="cipherx$")
+async def useUltroid(rs):
+    button = Button.inline("Start >>", "initft_2")
+    msg = await asst.send_message(
+        LOG_CHANNEL,
+        ULTSTRING,
+        file="https://telegra.ph/file/167a0b85048b04129bd3b.jpg",
+        buttons=button,
+    )
+    await eor(rs, f"**[Click Here]({msg.message_link})**")
