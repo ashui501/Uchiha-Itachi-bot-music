cc607f1d36cb81755f06d0e5aaa8c835d15dba11
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-12-20 04:29:01 +0330

    Update session.sh

--- resources/session/session.sh
+++ resources/session/session.sh
@@ -27,7 +27,7 @@ apt-get update
 apt-get upgrade -y
 pkg upgrade -y
 pkg install python wget -y
-wget https://raw.githubusercontent.com/TeamUltroid/ultroid/main/resources/session/ssgen.py
+wget https://raw.githubusercontent.com/ToxygenX/Megatron/main/resources/session/ssgen.py
 pip install telethon
 clear
 python3 ssgen.py
