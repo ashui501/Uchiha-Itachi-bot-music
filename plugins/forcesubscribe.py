8d9e02564c10a41243b659432b10eb802f2589bf
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-29 22:30:43 +0330

    Update forcesubscribe.py

--- plugins/forcesubscribe.py
+++ plugins/forcesubscribe.py
@@ -16,20 +16,27 @@
 
 import re
 
-from cython.dB.forcesub_db import add_forcesub, get_forcesetting, rem_forcesub
 from telethon.errors.rpcerrorlist import ChatAdminRequiredError, UserNotParticipantError
 from telethon.tl.custom import Button
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.functions.messages import ExportChatInviteRequest
+from telethon.tl.types import (
+    Channel,
+    ChannelParticipantBanned,
+    ChannelParticipantLeft,
+    User,
+)
+
+from CythonX.dB.forcesub_db import add_forcesub, get_forcesetting, rem_forcesub
 
 from . import (
     LOGS,
     asst,
     callback,
-    eor,
     events,
     get_string,
     in_pattern,
+    inline_mention,
     udB,
     ultroid_bot,
     ultroid_cmd,
@@ -38,57 +45,51 @@ from . import (
 CACHE = {}
 
 
-@ultroid_cmd(pattern="fsub ?(.*)", admins_only=True, groups_only=True)
+@ultroid_cmd(pattern="fsub( (.*)|$)", admins_only=True, groups_only=True)
 async def addfor(e):
-    match = e.pattern_match.group(1)
+    match = e.pattern_match.group(1).strip()
     if not match:
-        return await eor(e, get_string("fsub_1"), time=5)
-    if match.startswith("@"):
-        ch = match
-    else:
-        try:
-            ch = int(match)
-        except BaseException:
-            return await eor(e, get_string("fsub_2"), time=5)
+        return await e.eor(get_string("fsub_1"), time=5)
     try:
-        match = (await e.client.get_entity(ch)).id
+        match = await e.client.parse_id(match)
     except BaseException:
-        return await eor(e, get_string("fsub_2"), time=5)
-    if not str(match).startswith("-100"):
-        match = int("-100" + str(match))
+        return await e.eor(get_string("fsub_2"), time=5)
     add_forcesub(e.chat_id, match)
-    await eor(e, "Added ForceSub in This Chat !")
+    await e.eor("Added ForceSub in This Chat !")
+    ultroid_bot.add_handler(force_sub, events.NewMessage(incoming=True))
 
 
 @ultroid_cmd(pattern="remfsub$")
 async def remor(e):
     res = rem_forcesub(e.chat_id)
     if not res:
-        return await eor(e, get_string("fsub_3"), time=5)
-    await eor(e, "Removed ForceSub...")
+        return await e.eor(get_string("fsub_3"), time=5)
+    await e.eor("Removed ForceSub...")
 
 
 @ultroid_cmd(pattern="checkfsub$")
 async def getfsr(e):
     res = get_forcesetting(e.chat_id)
     if not res:
-        return await eor(e, "ForceSub is Not Active In This Chat !", time=5)
+        return await e.eor("ForceSub is Not Active In This Chat !", time=5)
     cha = await e.client.get_entity(int(res))
-    await eor(e, f"**ForceSub Status** : `Active`\n- **{cha.title}** `({res})`")
+    await e.eor(f"**ForceSub Status** : `Active`\n- **{cha.title}** `({res})`")
 
 
-@in_pattern("fsub ?(.*)", owner=True)
+@in_pattern("fsub( (.*)|$)", owner=True)
 async def fcall(e):
-    match = e.pattern_match.group(1)
+    match = e.pattern_match.group(1).strip()
     spli = match.split("_")
     user = await ultroid_bot.get_entity(int(spli[0]))
     cl = await ultroid_bot.get_entity(int(spli[1]))
-    text = f"Hi [{user.first_name}](tg://user?id={user.id}), You Need to Join"
+    text = f"Hi {inline_mention(user)}, You Need to Join"
     text += f" {cl.title} in order to Chat in this Group."
-    if not cl.username:
-        el = (await ultroid_bot(ExportChatInviteRequest(cl))).link
-    else:
-        el = "https://t.me/" + cl.username
+    el = (
+        f"https://t.me/{cl.username}"
+        if cl.username
+        else (await ultroid_bot(ExportChatInviteRequest(cl))).link
+    )
+
     res = [
         await e.builder.article(
             title="forcesub",
@@ -109,10 +110,14 @@ async def diesoon(e):
     if e.sender_id != int(spli[0]):
         return await e.answer(get_string("fsub_7"), alert=True)
     try:
-        await ultroid_bot(GetParticipantRequest(int(spli[1]), int(spli[0])))
+        values = await ultroid_bot(GetParticipantRequest(int(spli[1]), int(spli[0])))
+        if isinstance(values.participant, ChannelParticipantLeft) or (
+            isinstance(values.participant, ChannelParticipantBanned) and values.left
+        ):
+            raise UserNotParticipantError("")
     except UserNotParticipantError:
         return await e.answer(
-            "Please Join That Channel !\nThen Click This Button\n\n⚜️ CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ Ⲃⲟⲧ ⚜️", alert=True
+            "Please Join That Channel !\nThen Click This Button !", alert=True
         )
     await ultroid_bot.edit_permissions(
         e.chat_id, int(spli[0]), send_messages=True, until_date=None
@@ -120,14 +125,12 @@ async def diesoon(e):
     await e.edit(get_string("fsub_8"))
 
 
-@ultroid_bot.on(events.NewMessage(incoming=True))
-async def cacheahs(ult):
-    if not udB.get("FORCESUB"):
+async def force_sub(ult):
+    if not udB.get_key("FORCESUB"):
         return
-
     user = await ult.get_sender()
     joinchat = get_forcesetting(ult.chat_id)
-    if not joinchat or user.bot:
+    if (not joinchat) or (isinstance(user, User) and user.bot):
         return
     if CACHE.get(ult.chat_id):
         if CACHE[ult.chat_id].get(user.id):
@@ -138,7 +141,7 @@ async def cacheahs(ult):
         CACHE.update({ult.chat_id: {user.id: 1}})
     count = CACHE[ult.chat_id][user.id]
     if count == 11:
-        CACHE[ult.chat_id][user.id].update(1)
+        CACHE[ult.chat_id][user.id] = 1
         return
     if count in range(2, 11):
         return
@@ -147,11 +150,24 @@ async def cacheahs(ult):
         return
     except UserNotParticipantError:
         pass
+    if isinstance(user, Channel):
+        try:
+            await ultroid_bot.edit_permissions(
+                ult.chat_id, user.id, view_messages=False
+            )
+            return
+        except BaseException as er:
+            LOGS.exception(er)
     try:
         await ultroid_bot.edit_permissions(ult.chat_id, user.id, send_messages=False)
     except ChatAdminRequiredError:
         return
     except Exception as e:
+        await ult.delete()
         LOGS.info(e)
     res = await ultroid_bot.inline_query(asst.me.username, f"fsub {user.id}_{joinchat}")
     await res[0].click(ult.chat_id, reply_to=ult.id)
+
+
+if udB.get_key("FORCESUB"):
+    ultroid_bot.add_handler(force_sub, events.NewMessage(incoming=True))
