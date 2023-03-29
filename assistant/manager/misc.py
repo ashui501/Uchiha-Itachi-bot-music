75500395b404c5a90e866ce90f58b859752133fe
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-03-30 02:26:47 +0330

    Update misc.py

--- assistant/manager/misc.py
+++ assistant/manager/misc.py
@@ -1,44 +1,52 @@
-import aiohttp
 import random
-from cython.dB import DEVLIST
+
+import aiohttp
+
+from CythonX.dB import DEVLIST
+from CythonX.fns.admins import admin_check
+
 from . import *
 
 
-@ultroid_cmd(pattern="decide", type="manager")
+@asst_cmd(pattern="decide")
 async def dheh(e):
     text = ["Yes", "NoU", "Maybe", "IDK"]
     text = random.choice(text)
-    ri = e.id
-    if e.is_reply:
-        ri = e.reply_to_msg_id
+    ri = e.reply_to_msg_id or e.id
     await e.client.send_message(e.chat_id, text, reply_to=ri)
 
-@ultroid_cmd(pattern="echo ?(.*)", type=["manager"])
+
+@asst_cmd(pattern="echo( (.*)|$)")
 async def oqha(e):
-    match = e.pattern_match.group(1)
-    if match:
+    if not await admin_check(e):
+        return
+    if match := e.pattern_match.group(1).strip():
         text = match
         reply_to = e
     elif e.is_reply:
         text = (await e.get_reply_message()).text
         reply_to = e.reply_to_msg_id
     else:
-        return await eor(e, "What to Echo?", time=5)
+        return await e.eor("What to Echo?", time=5)
+    try:
+        await e.delete()
+    except BaseException as ex:
+        LOGS.error(ex)
     await e.client.send_message(e.chat_id, text, reply_to=reply_to)
 
 
-@ultroid_cmd(pattern="kickme$", type=["manager"], allow_all=True)
+@asst_cmd(pattern="kickme$")
 async def doit(e):
     if e.sender_id in DEVLIST:
         return await eod(e, "`I will Not Kick You, my Developer...`")
     try:
         await e.client.kick_participant(e.chat_id, e.sender_id)
     except Exception as Fe:
-        return await eor(e, str(Fe), time=5)
-    await eor(e, "Yes, You are right, get out.", time=5)
+        return await e.eor(str(Fe), time=5)
+    await e.eor("Yes, You are right, get out.", time=5)
 
 
-@ultroid_cmd(pattern="joke$", type=["manager"])
+@asst_cmd(pattern="joke$")
 async def do_joke(e):
     e = await e.get_reply_message() if e.is_reply else e
     link = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
