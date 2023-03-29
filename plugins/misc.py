42a5838e4eaa64f7eda8f1ba2d532853e64e553f
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-29 23:10:07 +0330

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,8 +1,8 @@
 """
 ✘ Commands Available -
 
-• `{i}eod` or `{i}eod <dd/mm>`
-    `Get Event of the Day`
+• `{i}eod`
+    `Get Event of the Today`
 
 • `{i}pntrst <link/id>`
     Download and send pinterest pins
@@ -18,89 +18,72 @@
 """
 
 import os
-from datetime import datetime
+from datetime import datetime as dt
 
 from bs4 import BeautifulSoup as bs
-from htmlwebshot import WebShot
-from img2html.converter import Img2HTMLConverter
-from requests import get
 
-from . import (
-    async_searcher,
-    eor,
-    fast_download,
-    get_random_user_data,
-    get_string,
-    re,
-    requests,
-    ultroid_cmd,
-)
+try:
+    from htmlwebshot import WebShot
+except ImportError:
+    WebShot = None
+try:
+    from img2html.converter import Img2HTMLConverter
+except ImportError:
+    Img2HTMLConverter = None
 
-_base = "https://pinterestdownloader.com/download?url="
+from . import async_searcher, get_random_user_data, get_string, re, ultroid_cmd
 
 
-def gib_link(link):
-    if link.startswith("https"):
-        return _base + link.replace(":", "%3A").replace("/", "%2F")
-    return _base + f"https%3A%2F%2Fpin.it%2F{link}"
-
-
-@ultroid_cmd(pattern="eod ?(.*)")
+@ultroid_cmd(pattern="eod$")
 async def diela(e):
-    match = e.pattern_match.group(1)
-    m = await eor(e, get_string("com_1"))
+    m = await e.eor(get_string("com_1"))
     li = "https://daysoftheyear.com"
     te = "🎊 **Events of the Day**\n\n"
-    if match:
-        date = match.split("/")[0]
-        month = match.split("/")[1]
-        li += "/days/2021/" + month + "/" + date
-        te = get_string("eod_2").format(match)
-    else:
-        da = datetime.today().strftime("%F").split("-")
-        li += "/days/2021/" + da[1] + "/" + da[2]
-    ct = requests.get(li).content
+    da = dt.now()
+    month = da.strftime("%b")
+    li += f"/days/{month}/" + da.strftime("%F").split("-")[2]
+    ct = await async_searcher(li, re_content=True)
     bt = bs(ct, "html.parser", from_encoding="utf-8")
     ml = bt.find_all("a", "js-link-target", href=re.compile("daysoftheyear.com/days"))
     for eve in ml[:5]:
-        te += "• " + f'[{eve.text}]({eve["href"]})\n'
+        te += f'• [{eve.text}]({eve["href"]})\n'
     await m.edit(te, link_preview=False)
 
 
 @ultroid_cmd(
-    pattern="pntrst ?(.*)",
+    pattern="pntrst( (.*)|$)",
 )
 async def pinterest(e):
-    m = e.pattern_match.group(1)
-    get_link = get(gib_link(m)).text
-    hehe = bs(get_link, "html.parser")
-    hulu = hehe.find_all("a", {"class": "download_button"})
-    if len(hulu) < 1:
-        await eor(e, "`Wrong link or private pin.`", time=5)
-    elif len(hulu) > 1:
-        video = await fast_download(hulu[0]["href"])
-        thumb = await fast_download(hulu[1]["href"])
-        await e.delete()
-        await e.client.send_file(e.chat_id, video, thumb=thumb, caption=f"Pin:- {m}")
-        [os.remove(file) for file in [video, thumb]]
-    else:
-        await e.delete()
-        await e.client.send_file(e.chat_id, hulu[0]["href"], caption=f"Pin:- {m}")
+    m = e.pattern_match.group(1).strip()
+    if not m:
+        return await e.eor("`Give pinterest link.`", time=3)
+    soup = await async_searcher(
+        "https://www.expertstool.com/download-pinterest-video/",
+        data={"url": m},
+        post=True,
+    )
+    try:
+        _soup = bs(soup, "html.parser").find("table").tbody.find_all("tr")
+    except BaseException:
+        return await e.eor("`Wrong link or private pin.`", time=5)
+    file = _soup[1] if len(_soup) > 1 else _soup[0]
+    file = file.td.a["href"]
+    await e.client.send_file(e.chat_id, file, caption=f"Pin:- {m}")
 
 
-@ultroid_cmd(pattern="gadget ?(.*)")
+@ultroid_cmd(pattern="gadget( (.*)|$)")
 async def mobs(e):
-    mat = e.pattern_match.group(1)
+    mat = e.pattern_match.group(1).strip()
     if not mat:
-        await eor(e, "Please Give a Mobile Name to look for.")
+        await e.eor("Please Give a Mobile Name to look for.")
     query = mat.replace(" ", "%20")
     jwala = f"https://gadgets.ndtv.com/search?searchtext={query}"
     c = await async_searcher(jwala)
     b = bs(c, "html.parser", from_encoding="utf-8")
     co = b.find_all("div", "rvw-imgbox")
     if not co:
-        return await eor(e, "No Results Found!")
-    bt = await eor(e, get_string("com_1"))
+        return await e.eor("No Results Found!")
+    bt = await e.eor(get_string("com_1"))
     out = "**📱 Mobile / Gadgets Search**\n\n"
     li = co[0].find("a")
     imu, title = None, li.find("img")["title"]
@@ -117,27 +100,31 @@ async def mobs(e):
         ty = fp.findNext()
         out += f"- **{ty.text}** - `{ty.findNext().text}`\n"
     out += "_"
-    await e.reply(out, file=imu)
+    if imu == []:
+        imu = None
+    await e.reply(out, file=imu, link_preview=False)
     await bt.delete()
 
 
 @ultroid_cmd(pattern="randomuser")
 async def _gen_data(event):
-    x = await eor(event, get_string("com_1"))
+    x = await event.eor(get_string("com_1"))
     msg, pic = await get_random_user_data()
     await event.reply(file=pic, message=msg)
     await x.delete()
 
 
 @ultroid_cmd(
-    pattern="ascii ?(.*)",
+    pattern="ascii( (.*)|$)",
 )
 async def _(e):
+    if not Img2HTMLConverter:
+        return await e.eor("'img2html-converter' not installed!")
     if not e.reply_to_msg_id:
-        return await eor(e, get_string("ascii_1"))
-    m = await eor(e, get_string("ascii_2"))
+        return await e.eor(get_string("ascii_1"))
+    m = await e.eor(get_string("ascii_2"))
     img = await (await e.get_reply_message()).download_media()
-    char = "■" if not e.pattern_match.group(1) else e.pattern_match.group(1)
+    char = e.pattern_match.group(1).strip() or "■"
     converter = Img2HTMLConverter(char=char)
     html = converter.convert(img)
     shot = WebShot(quality=85)
