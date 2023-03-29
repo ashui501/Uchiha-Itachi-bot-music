85852cee52c5dcfc1e0ff5f876481200348f5cc9
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-03-29 23:07:24 +0330

    Update __init__.py

--- assistant/__init__.py
+++ assistant/__init__.py
@@ -1,22 +1,25 @@
-from cython import *
-from cython.functions.helper import *
-from cython.misc import owner_and_sudos
-from cython.misc._assistant import asst_cmd, callback, in_pattern
 from telethon import Button, custom
 
-from plugins import ATRA_COL
-from strings import get_languages, get_string, language
+from plugins import ATRA_COL, InlinePlugin
+from CythonX import *
+from CythonX import _ult_cache
+from CythonX._misc import owner_and_sudos
+from CythonX._misc._assistant import asst_cmd, callback, in_pattern
+from CythonX.fns.helper import *
+from CythonX.fns.tools import get_stored_file
+from strings import get_languages, get_string
 
-OWNER_NAME = ultroid_bot.me.first_name
-OWNER_ID = ultroid_bot.me.id
+OWNER_NAME = ultroid_bot.full_name
+OWNER_ID = ultroid_bot.uid
 
 AST_PLUGINS = {}
 
 
 async def setit(event, name, value):
     try:
-        udB.set(name, value)
-    except BaseException:
+        udB.set_key(name, value)
+    except BaseException as er:
+        LOGS.exception(er)
         return await event.edit("`Something Went Wrong`")
 
 
