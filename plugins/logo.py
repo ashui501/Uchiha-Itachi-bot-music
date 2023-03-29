5ac44d950c0d7ecfb5b4d2a495017be7bfbc8311
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-29 23:03:49 +0330

    Update logo.py

--- plugins/logo.py
+++ plugins/logo.py
@@ -11,43 +11,54 @@ import glob
 import os
 import random
 
-from PIL import Image, ImageDraw, ImageFont
-from cython.functions.misc import unsplashsearch
 from telethon.tl.types import InputMessagesFilterPhotos
 
-from . import (
-    OWNER_ID,
-    OWNER_NAME,
-    download_file,
-    eor,
-    get_string,
-    mediainfo,
-    ultroid_cmd,
-)
+try:
+    from PIL import Image
+except ImportError:
+    Image = None
+from CythonX.fns.misc import unsplashsearch
+from CythonX.fns.tools import LogoHelper
+
+from . import OWNER_ID, OWNER_NAME, download_file, get_string, mediainfo, ultroid_cmd
 
 
-@ultroid_cmd(pattern="logo ?(.*)")
+@ultroid_cmd(pattern="logo( (.*)|$)")
 async def logo_gen(event):
-    xx = await eor(event, get_string("com_1"))
-    name = event.pattern_match.group(1)
+    xx = await event.eor(get_string("com_1"))
+    name = event.pattern_match.group(1).strip()
     if not name:
-        await eor(xx, "`Give a name too!`", time=5)
+        return await xx.eor("`Give a name too!`", time=5)
     bg_, font_ = None, None
     if event.reply_to_msg_id:
         temp = await event.get_reply_message()
         if temp.media:
-            if hasattr(temp.media, "document"):
-                if "font" in temp.file.mime_type:
-                    font_ = await temp.download_media()
-                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
-                    font_ = await temp.download_media()
+            if hasattr(temp.media, "document") and (
+                ("font" in temp.file.mime_type)
+                or (".ttf" in temp.file.name)
+                or (".otf" in temp.file.name)
+            ):
+                font_ = await temp.download_media("resources/fonts/")
             elif "pic" in mediainfo(temp.media):
                 bg_ = await temp.download_media()
     if not bg_:
         if event.client._bot:
-            SRCH = ["blur background", "background", "neon lights", "wallpaper"]
+            SRCH = [
+                "blur background",
+                "background",
+                "neon lights",
+                "nature",
+                "abstract",
+                "space",
+                "3d render",
+            ]
             res = await unsplashsearch(random.choice(SRCH), limit=1)
-            bg_ = await download_file(res[0], "resources/downloads/logo.png")
+            bg_, _ = await download_file(res[0], "resources/downloads/logo.png")
+            newimg = "resources/downloads/unsplash-temp.jpg"
+            img_ = Image.open(bg_)
+            img_.resize((5000, 5000)).save(newimg)
+            os.remove(bg_)
+            bg_ = newimg
         else:
             pics = []
             async for i in event.client.iter_messages(
@@ -61,45 +72,27 @@ async def logo_gen(event):
         fpath_ = glob.glob("resources/fonts/*")
         font_ = random.choice(fpath_)
     if len(name) <= 8:
-        fnt_size = 150
         strke = 10
     elif len(name) >= 9:
-        fnt_size = 50
         strke = 5
     else:
-        fnt_size = 130
         strke = 20
-    img = Image.open(bg_)
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(font_, fnt_size)
-    w, h = draw.textsize(name, font=font)
-    h += int(h * 0.21)
-    image_width, image_height = img.size
-    draw.text(
-        ((image_width - w) / 2, (image_height - h) / 2),
+    name = LogoHelper.make_logo(
+        bg_,
         name,
-        font=font,
-        fill=(255, 255, 255),
+        font_,
+        fill="white",
+        stroke_width=strke,
+        stroke_fill="black",
     )
-    x = (image_width - w) / 2
-    y = (image_height - h) / 2
-    draw.text(
-        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
+    await xx.edit("`Done!`")
+    await event.client.send_file(
+        event.chat_id,
+        file=name,
+        caption=f"Logo by [{OWNER_NAME}](tg://user?id={OWNER_ID})",
+        force_document=True,
     )
-    flnme = "cipherx.png"
-    img.save(flnme, "png")
-    await xx.edit("`☑️`")
-    await xx.edit("`✅`")
-    if os.path.exists(flnme):
-        await event.client.send_file(
-            event.chat_id,
-            file=flnme,
-            caption=f"𑀉᧐g᧐ ⲙᥲɗᥱ δy CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ Ⲃⲟⲧ",
-            force_document=False,
-        )
-        os.remove(flnme)
-        await xx.delete()
+    os.remove(name)
+    await xx.delete()
     if os.path.exists(bg_):
         os.remove(bg_)
-    if os.path.exists(font_) and not font_.startswith("resources/fonts"):
-        os.remove(font_)
