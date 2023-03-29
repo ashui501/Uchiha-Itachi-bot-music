d41367aa80b9439ac666066663106ac7f1b0055a
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-03-29 23:10:41 +0330

    Update games.py

--- assistant/games.py
+++ assistant/games.py
@@ -1,6 +1,6 @@
 """
 • `{i}akinator` | `/akinator`
-   Start akinator game from Assistant
+   Start akinator game from Userbot/Assistant
 
 • `/startgame`
    Open Portal for Games
@@ -13,11 +13,14 @@ import uuid
 from html import unescape
 from random import choice, shuffle
 
-import akinator
-from cython.functions.helper import inline_mention
-from cython.functions.tools import async_searcher
-from cython.misc import owner_and_sudos
-from cython.misc._decorators import ultroid_cmd
+from . import LOGS
+
+try:
+    import akinator
+except ImportError:
+    akinator = None
+    LOGS.error("'akinator' not installed.")
+
 from telethon.errors.rpcerrorlist import (
     BotMethodInvalidError,
     ChatSendStickersForbiddenError,
@@ -25,36 +28,47 @@ from telethon.errors.rpcerrorlist import (
 from telethon.events import Raw
 from telethon.tl.types import InputMediaPoll, Poll, PollAnswer, UpdateMessagePollVote
 
+from CythonX._misc._decorators import ultroid_cmd
+from CythonX.fns.helper import inline_mention
+from CythonX.fns.tools import async_searcher
+
 from . import *
 
 # -------------------------- Akinator ----------------------- #
 
 games = {}
-aki_photo = "https://telegra.ph/file/3cc8825c029fd0cab9edc.jpg"
+aki_photo = "https://graph.org/file/3cc8825c029fd0cab9edc.jpg"
 
 
-@ultroid_cmd(
-    pattern="akinator", type=["official", "manager"], admins_only=True 
-)
-async def doit(e):
+@ultroid_cmd(pattern="akinator")
+async def akina(e):
+    if not akinator:
+        return
     sta = akinator.Akinator()
     games.update({e.chat_id: {e.id: sta}})
     try:
         m = await e.client.inline_query(asst.me.username, f"aki_{e.chat_id}_{e.id}")
         await m[0].click(e.chat_id)
     except BotMethodInvalidError:
-        return await asst.send_file(
+        await asst.send_file(
             e.chat_id,
             aki_photo,
             buttons=Button.inline(get_string("aki_2"), data=f"aki_{e.chat_id}_{e.id}"),
         )
+    except Exception as er:
+        return await e.eor(f"**ERROR :** `{er}`")
     if e.out:
         await e.delete()
 
 
-@callback(re.compile("aki_?(.*)"), owner=True)
+@asst_cmd(pattern="akinator", owner=True)
+async def _akokk(e):
+    await akina(e)
+
+
+@callback(re.compile("aki_(.*)"), owner=True)
 async def doai(e):
-    adt = e.pattern_match.group(1).decode("utf-8")
+    adt = e.pattern_match.group(1).strip().decode("utf-8")
     dt = adt.split("_")
     ch = int(dt[0])
     mid = int(dt[1])
@@ -69,10 +83,10 @@ async def doai(e):
 
     bts = [bts, cts]
     # ignored Back Button since it makes the Pagination looks Bad
-    await e.edit("Q. " + qu, buttons=bts)
+    await e.edit(f"Q. {qu}", buttons=bts)
 
 
-@callback(re.compile("aka_?(.*)"), owner=True)
+@callback(re.compile("aka_(.*)"), owner=True)
 async def okah(e):
     mk = e.pattern_match.group(1).decode("utf-8").split("_")
     ch = int(mk[0])
@@ -118,16 +132,23 @@ async def eiagx(e):
 
 # ----------------------- Main Command ------------------- #
 
+GIMAGES = [
+    "https://graph.org/file/1c51015bae5205a65fd69.jpg",
+    "https://imgwhale.xyz/3xyr322l64j9590",
+]
 
-@ultroid_cmd(
-    pattern="startgame", type=["official", "manager"], admins_only=True
-)
+
+@asst_cmd(pattern="startgame", owner=True)
 async def magic(event):
     buttons = [
         [Button.inline("Trivia Quiz", "trzia")],
         [Button.inline("Cancel ❌", "delit")],
     ]
-    await event.reply("Choose The Game 🎮", buttons=buttons)
+    await event.reply(
+        get_string("games_1"),
+        file=choice(GIMAGES),
+        buttons=buttons,
+    )
 
 
 # -------------------------- Trivia ----------------------- #
@@ -142,10 +163,12 @@ CONGO_STICKER = [
     "CAADAgADpQAD9wLID-xfZCDwOI5LAg",
     "CAADAgADjAADECECEFZM-SrKO9GgAg",
     "CAADAgADSwIAAj-VzArAzNCDiGWAHAI",
+    "CAADAgADhQADwZxgDIuMHR9IU10iAg",
+    "CAADAgADiwMAAsSraAuoe2BwYu1sdQI",
 ]
 
 
-@callback("delit")
+@callback("delit", owner=True)
 async def delete_it(event):
     await event.delete()
 
@@ -153,7 +176,7 @@ async def delete_it(event):
 @callback(re.compile("ctdown(.*)"), owner=True)
 async def ct_spam(e):
     n = e.data_match.group(1).decode("utf-8")
-    await e.answer(f"Wait {n} seconds...", alert=True)
+    await e.answer(f"Wait {n} seconds..", alert=True)
 
 
 @callback(re.compile("trzia(.*)"), owner=True)
@@ -179,28 +202,26 @@ async def choose_cata(event):
                 buttons.append((btt[-1],))
             buttons.append([Button.inline("Cancel ❌", "delit")])
             TR_BTS.update({"category": buttons})
-        text = "Choose Category!"
+        text = get_string("games_2")
     elif match[0] == "d":
         cat = match[1:]
         buttons = [[Button.inline(i, f"trziac{cat}_{i}") for i in DIFI_KEYS]]
         buttons.append(get_back_button("trzia"))
-        text = "Choose Difficulty Level"
+        text = get_string("games_3")
     elif match[0] == "c":
         m = match[1:]
         buttons = [[Button.inline(str(i), f"trziat{m}_{i}") for i in range(10, 70, 20)]]
-        text = "Choose Number of Questions..."
+        text = get_string("games_4")
     elif match[0] == "t":
         m_ = match[1:]
         buttons = [
             [Button.inline(str(i), f"trzias{m_}_{i}") for i in [10, 30, 60, 120]]
         ]
-        text = "Choose Time Interval..."
+        text = get_string("games_5")
     elif match[0] == "s":
         chat = event.chat_id
         cat, le, nu, in_ = match[2:].split("_")
-        msg = await event.edit(
-            f"**• Starting Quiz in 5secs.** \n**• Level :** {le}\n**• Qs :** {nu}"
-        )
+        msg = await event.edit(get_string("games_6").format(le, nu))
         for i in reversed(range(5)):
             msg = await msg.edit(buttons=Button.inline(f"{i} ⏰", f"ctdown{i}"))
             await asyncio.sleep(1)
@@ -213,7 +234,7 @@ async def choose_cata(event):
         )
         qs = qsss["results"]
         if not qs:
-            await event.respond("Sorry, No Question Found for the given Criteria...")
+            await event.respond("Sorry, No Question Found for the given Criteria..")
             await event.delete()
             return
         TRIVIA_CHATS.update({chat: {}})
@@ -239,7 +260,7 @@ async def choose_cata(event):
                     close_period=int(in_),
                 ),
                 correct_answers=[ansi],
-                solution="✵ CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ Ⲃⲟⲧ ✵",
+                solution="CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ ⲃⲟⲧ",
                 solution_entities=[],
             )
             m_ = await event.client.send_message(chat, file=poll)
@@ -277,7 +298,7 @@ async def choose_cata(event):
     Raw(UpdateMessagePollVote, func=lambda x: TRIVIA_CHATS and POLLS.get(x.poll_id))
 )
 async def pollish(eve):
-    if not POLLS.get(eve.poll_id)["chat"] in TRIVIA_CHATS.keys():
+    if POLLS.get(eve.poll_id)["chat"] not in TRIVIA_CHATS.keys():
         return
     if POLLS[eve.poll_id]["answer"] != eve.options[0]:
         return
@@ -289,9 +310,7 @@ async def pollish(eve):
         TRIVIA_CHATS[chat][user] += 1
 
 
-@ultroid_cmd(
-    pattern="cancel", admins_only=True, type=["official", "manager"], func=lambda x: TRIVIA_CHATS.get(x.chat_id) 
-)
+@asst_cmd("cancel", owner=True, func=lambda x: TRIVIA_CHATS.get(x.chat_id))
 async def cancelish(event):
     chat = TRIVIA_CHATS.get(event.chat_id)
     chat.update({"cancel": True})
