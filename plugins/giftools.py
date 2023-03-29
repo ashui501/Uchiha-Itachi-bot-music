30761ff2d928da991dd705a56b3d1910c7627136
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-29 22:52:40 +0330

    Update giftools.py

--- plugins/giftools.py
+++ plugins/giftools.py
@@ -7,6 +7,9 @@
 •`{i}bwgif`
   Make Gif black and white
 
+•`{i}rvgif`
+  Reverse a gif
+
 •`{i}vtog`
   Reply To Video , It will Create Gif
   Video to Gif
@@ -19,54 +22,51 @@ import random
 import time
 from datetime import datetime as dt
 
-from . import HNDLR, LOGS, bash, downloader, eor, get_string, mediainfo, ultroid_cmd
+from . import HNDLR, LOGS, bash, downloader, get_string, mediainfo, ultroid_cmd
 
 
-@ultroid_cmd(pattern="bwgif$")
+@ultroid_cmd(pattern="(bw|invert)gif$")
 async def igif(e):
+    match = e.pattern_match.group(1).strip()
     a = await e.get_reply_message()
     if not (a and a.media):
-        return await eor(e, "`Reply To gif only`", time=5)
+        return await e.eor("`Reply To gif only`", time=5)
     wut = mediainfo(a.media)
     if "gif" not in wut:
-        return await eor(e, "`Reply To Gif Only`", time=5)
-    xx = await eor(e, get_string("com_1"))
+        return await e.eor("`Reply To Gif Only`", time=5)
+    xx = await e.eor(get_string("com_1"))
     z = await a.download_media()
+    if match == "bw":
+        cmd = f'ffmpeg -i "{z}" -vf format=gray ult.gif -y'
+    else:
+        cmd = f'ffmpeg -i "{z}" -vf lutyuv="y=negval:u=negval:v=negval" ult.gif -y'
     try:
-        await bash(f'ffmpeg -i "{z}" -vf format=gray cipherx.gif -y')
-        await e.client.send_file(e.chat_id, "cipherx.gif", support_stream=True)
+        await bash(cmd)
+        await e.client.send_file(e.chat_id, "ult.gif", supports_streaming=True)
         os.remove(z)
-        os.remove("cipherx.gif")
+        os.remove("ult.gif")
         await xx.delete()
     except Exception as er:
         LOGS.info(er)
 
 
-@ultroid_cmd(pattern="invertgif$")
-async def igif(e):
-    a = await e.get_reply_message()
-    if not (a and a.media):
-        return await eor(e, "`Reply To gif only`", time=5)
-    wut = mediainfo(a.media)
-    if "gif" not in wut:
-        return await eor(e, "`Reply To Gif Only`", time=5)
-    xx = await eor(e, get_string("com_1"))
-    z = await a.download_media()
-    try:
-        await bash(
-            f'ffmpeg -i "{z}" -vf lutyuv="y=negval:u=negval:v=negval" cipherx.gif -y'
-        )
-        await e.client.send_file(e.chat_id, "cipherx.gif", support_stream=True)
-        os.remove(z)
-        os.remove("cipherx.gif")
-        await xx.delete()
-    except Exception as er:
-        LOGS.info(er)
+@ultroid_cmd(pattern="rvgif$")
+async def reverse_gif(event):
+    a = await event.get_reply_message()
+    if not (a and a.media) and "video" not in mediainfo(a.media):
+        return await event.eor("`Reply To Video only`", time=5)
+    msg = await event.eor(get_string("com_1"))
+    file = await a.download_media()
+    await bash(f'ffmpeg -i "{file}" -vf reverse -af areverse reversed.mp4 -y')
+    await event.respond("- **Reversed Video/GIF**", file="reversed.mp4")
+    await msg.delete()
+    os.remove(file)
+    os.remove("reversed.mp4")
 
 
-@ultroid_cmd(pattern="gif ?(.*)")
+@ultroid_cmd(pattern="gif( (.*)|$)")
 async def gifs(ult):
-    get = ult.pattern_match.group(1)
+    get = ult.pattern_match.group(1).strip()
     xx = random.randint(0, 5)
     n = 0
     if ";" in get:
@@ -75,8 +75,8 @@ async def gifs(ult):
         except IndexError:
             pass
     if not get:
-        return await eor(ult, f"`{HNDLR}gif <query>`")
-    m = await eor(ult, get_string("com_2"))
+        return await ult.eor(f"`{HNDLR}gif <query>`")
+    m = await ult.eor(get_string("com_2"))
     gifs = await ult.client.inline_query("gif", get)
     if not n:
         await gifs[xx].click(
@@ -94,17 +94,17 @@ async def gifs(ult):
 async def vtogif(e):
     a = await e.get_reply_message()
     if not (a and a.media):
-        return await eor(e, "`Reply To video only`", time=5)
+        return await e.eor("`Reply To video only`", time=5)
     wut = mediainfo(a.media)
     if "video" not in wut:
-        return await eor(e, "`Reply To Video Only`", time=5)
-    xx = await eor(e, get_string("com_1"))
+        return await e.eor("`Reply To Video Only`", time=5)
+    xx = await e.eor(get_string("com_1"))
     dur = a.media.document.attributes[0].duration
     tt = time.time()
     if int(dur) < 120:
         z = await a.download_media()
         await bash(
-            f'ffmpeg -i {z} -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 cipherx.gif -y'
+            f'ffmpeg -i {z} -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 ult.gif -y'
         )
     else:
         filename = a.file.name
@@ -113,10 +113,10 @@ async def vtogif(e):
         vid = await downloader(filename, a.media.document, xx, tt, get_string("com_5"))
         z = vid.name
         await bash(
-            f'ffmpeg -ss 3 -t 100 -i {z} -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 cipherx.gif'
+            f'ffmpeg -ss 3 -t 100 -i {z} -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 ult.gif'
         )
 
-    await e.client.send_file(e.chat_id, "cipherx.gif", support_stream=True)
+    await e.client.send_file(e.chat_id, "ult.gif", support_stream=True)
     os.remove(z)
-    os.remove("cipherx.gif")
+    os.remove("ult.gif")
     await xx.delete()
