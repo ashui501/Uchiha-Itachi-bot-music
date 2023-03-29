824096fc142f55cbc56b9225b04e986d9a3850d0
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-29 21:53:53 +0330

    Update blacklist.py

--- plugins/blacklist.py
+++ plugins/blacklist.py
@@ -1,73 +1,62 @@
-"""
-✘ Commands Available -
+from . import get_help
 
-• `{i}blacklist <word/all words with a space>`
-    blacklist the choosen word in that chat.
+__doc__ = get_help("help_blacklist")
 
-• `{i}remblacklist <word>`
-    Remove the word from blacklist..
 
-• `{i}listblacklist`
-    list all blacklisted words.
-
-  'if a person uses blacklist Word his/her message will be deleted'
-  'And you Must be Admin in that Chat'
-"""
-
-from cython.dB.blacklist_db import (
+from CythonX.dB.blacklist_db import (
     add_blacklist,
     get_blacklist,
     list_blacklist,
     rem_blacklist,
 )
 
-from . import eor, events, get_string, ultroid_bot, ultroid_cmd
+from . import events, get_string, udB, ultroid_bot, ultroid_cmd
 
 
-@ultroid_cmd(pattern="blacklist ?(.*)", admins_only=True)
+@ultroid_cmd(pattern="blacklist( (.*)|$)", admins_only=True)
 async def af(e):
-    wrd = e.pattern_match.group(1)
+    wrd = e.pattern_match.group(1).strip()
     chat = e.chat_id
     if not (wrd):
-        return await eor(e, get_string("blk_1"), time=5)
+        return await e.eor(get_string("blk_1"), time=5)
     wrd = e.text[11:]
     heh = wrd.split(" ")
     for z in heh:
         add_blacklist(int(chat), z.lower())
-    await eor(e, get_string("blk_2").format(wrd))
+    ultroid_bot.add_handler(blacklist, events.NewMessage(incoming=True))
+    await e.eor(get_string("blk_2").format(wrd))
 
 
-@ultroid_cmd(pattern="remblacklist ?(.*)", admins_only=True)
+@ultroid_cmd(pattern="remblacklist( (.*)|$)", admins_only=True)
 async def rf(e):
-    wrd = e.pattern_match.group(1)
+    wrd = e.pattern_match.group(1).strip()
     chat = e.chat_id
     if not wrd:
-        return await eor(e, get_string("blk_3"), time=5)
+        return await e.eor(get_string("blk_3"), time=5)
     wrd = e.text[14:]
     heh = wrd.split(" ")
     for z in heh:
         rem_blacklist(int(chat), z.lower())
-    await eor(e, get_string("blk_4").format(wrd))
+    await e.eor(get_string("blk_4").format(wrd))
 
 
 @ultroid_cmd(pattern="listblacklist$", admins_only=True)
 async def lsnote(e):
-    x = list_blacklist(e.chat_id)
-    if x:
+    if x := list_blacklist(e.chat_id):
         sd = get_string("blk_5")
-        return await eor(e, sd + x)
-    await eor(e, get_string("blk_6"))
+        return await e.eor(sd + x)
+    await e.eor(get_string("blk_6"))
 
 
-@ultroid_bot.on(events.NewMessage(incoming=True))
-async def bl(e):
-    x = get_blacklist(e.chat_id)
-    if x:
-        for z in e.text.lower().split():
-            for zz in x:
-                if z == zz:
-                    try:
-                        await e.delete()
-                        break
-                    except BaseException:
-                        break
+async def blacklist(e):
+    if x := get_blacklist(e.chat_id):
+        text = e.text.lower().split()
+        if any((z in text) for z in x):
+            try:
+                await e.delete()
+            except BaseException:
+                pass
+
+
+if udB.get_key("BLACKLIST_DB"):
+    ultroid_bot.add_handler(blacklist, events.NewMessage(incoming=True))
