be9e96a13aa4e37df69ca6101af63cd60d22df50
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-03-29 23:14:29 +0330

    Create initial.py

---
+++ assistant/initial.py
@@ -0,0 +1,49 @@
+import re
+
+from . import *
+
+STRINGS = {
+    1: """HI
+OK BYE
+""",
+}
+
+
+@callback(re.compile("initft_(\\d+)"))
+async def init_depl(e):
+    CURRENT = int(e.data_match.group(1))
+    if CURRENT == 5:
+        return await e.edit(
+            STRINGS[5],
+            buttons=Button.inline("<< Back", "initbk_4"),
+            link_preview=False,
+        )
+
+    await e.edit(
+        STRINGS[CURRENT],
+        buttons=[
+            Button.inline("<<", f"initbk_{str(CURRENT - 1)}"),
+            Button.inline(">>", f"initft_{str(CURRENT + 1)}"),
+        ],
+        link_preview=False,
+    )
+
+
+@callback(re.compile("initbk_(\\d+)"))
+async def ineiq(e):
+    CURRENT = int(e.data_match.group(1))
+    if CURRENT == 1:
+        return await e.edit(
+            STRINGS[1],
+            buttons=Button.inline("Start Back >>", "initft_2"),
+            link_preview=False,
+        )
+
+    await e.edit(
+        STRINGS[CURRENT],
+        buttons=[
+            Button.inline("<<", f"initbk_{str(CURRENT - 1)}"),
+            Button.inline(">>", f"initft_{str(CURRENT + 1)}"),
+        ],
+        link_preview=False,
+    )
