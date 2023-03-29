ea810f07edff264a063d6a279f6d66455280c54e
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-29 21:46:19 +0330

    Update asst_cmd.py

--- plugins/asst_cmd.py
+++ plugins/asst_cmd.py
@@ -1,31 +1,27 @@
-"""
-✘ Commands Available -
+from . import get_help
 
-•`{i}addcmd <new cmd> <reply>`
-   It will set new cmd for your assistant bot with that reply message.
+__doc__ = get_help("help_asstcmd")
 
-•`{i}remcmd <cmd name>`
-   It will remove your cmd.
-
-•`{i}listcmd`
-   To Get list of all your custom cmd.
-"""
 import os
 
-from cython.dB.asstcmd_db import *
-from cython.functions.tools import create_tl_btn, format_btn, get_msg_button
-from telegraph import upload_file as uf
-from telethon.utils import pack_bot_file_id
+from CythonX.dB.asstcmd_db import add_cmd, cmd_reply, list_cmds, rem_cmd
+from CythonX.fns.tools import create_tl_btn, format_btn, get_msg_button
 
-from . import asst_cmd, eor, get_string, mediainfo, ultroid_cmd
+try:
+    from telegraph import upload_file as uf
+except ImportError:
+    uf = None
+from telethon import events, utils
+
+from . import asst, get_string, mediainfo, udB, ultroid_cmd
 
 
-@ultroid_cmd(pattern="addcmd ?(.*)")
+@ultroid_cmd(pattern="addcmd( (.*)|$)")
 async def ac(e):
-    wrd = (e.pattern_match.group(1)).lower()
+    wrd = (e.pattern_match.group(1).strip()).lower()
     wt = await e.get_reply_message()
     if not (wt and wrd):
-        return await eor(e, get_string("asstcmd_1"), time=5)
+        return await e.eor(get_string("asstcmd_1"), time=5)
     if "/" in wrd:
         wrd = wrd.replace("/", "")
     btn = format_btn(wt.buttons) if wt.buttons else None
@@ -35,39 +31,45 @@ async def ac(e):
             dl = await e.client.download_media(wt.media)
             variable = uf(dl)
             os.remove(dl)
-            m = "https://telegra.ph" + variable[0]
+            m = f"https://graph.org{variable[0]}"
         elif wut == "video":
             if wt.media.document.size > 8 * 1000 * 1000:
-                return await eor(e, get_string("com_4"), time=5)
+                return await e.eor(get_string("com_4"), time=5)
             dl = await e.client.download_media(wt.media)
             variable = uf(dl)
             os.remove(dl)
-            m = "https://telegra.ph" + variable[0]
+            m = f"https://graph.org{variable[0]}"
         else:
-            m = pack_bot_file_id(wt.media)
+            m = utils.pack_bot_file_id(wt.media)
         if wt.text:
             txt = wt.text
             if not btn:
                 txt, btn = get_msg_button(wt.text)
             add_cmd(wrd, txt, m, btn)
         else:
-            add_cmd(wrd, None, m)
+            add_cmd(wrd, None, m, btn)
     else:
         txt = wt.text
         if not btn:
             txt, btn = get_msg_button(wt.text)
         add_cmd(wrd, txt, None, btn)
-    await eor(e, get_string("asstcmd_4").format(wrd))
+    asst.add_handler(
+        astcmds,
+        events.NewMessage(
+            func=lambda x: x.text.startswith("/") and x.text[1:] in list(list_cmds())
+        ),
+    )
+    await e.eor(get_string("asstcmd_4").format(wrd))
 
 
-@ultroid_cmd(pattern="remcmd ?(.*)")
+@ultroid_cmd(pattern="remcmd( (.*)|$)")
 async def rc(e):
-    wrd = (e.pattern_match.group(1)).lower()
+    wrd = (e.pattern_match.group(1).strip()).lower()
     if not wrd:
-        return await eor(e, get_string("asstcmd_2"), time=5)
+        return await e.eor(get_string("asstcmd_2"), time=5)
     wrd = wrd.replace("/", "")
     rem_cmd(wrd)
-    await eor(e, get_string("asstcmd_3").format(wrd))
+    await e.eor(get_string("asstcmd_3").format(wrd))
 
 
 @ultroid_cmd(pattern="listcmd$")
@@ -75,16 +77,24 @@ async def lscmd(e):
     if list_cmds():
         ok = get_string("asstcmd_6")
         for x in list_cmds():
-            ok += "/" + x + "\n"
-        return await eor(e, ok)
-    return await eor(e, get_string("asstcmd_5"))
+            ok += f"/{x}" + "\n"
+        return await e.eor(ok)
+    return await e.eor(get_string("asstcmd_5"))
 
 
-@asst_cmd(func=lambda x: x.text.startswith("/") and x.text[1:] in list(list_cmds()))
-async def ascmds(e):
+async def astcmds(e):
     xx = (e.text.replace("/", "")).lower().split()[0]
     if cmd_reply(xx):
         msg, media, bt = cmd_reply(xx)
         if bt:
             bt = create_tl_btn(bt)
         await e.reply(msg, file=media, buttons=bt)
+
+
+if udB.get_key("ASST_CMDS"):
+    asst.add_handler(
+        astcmds,
+        events.NewMessage(
+            func=lambda x: x.text.startswith("/") and x.text[1:] in list(list_cmds())
+        ),
+    )
