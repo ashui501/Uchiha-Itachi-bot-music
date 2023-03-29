fa902b55c4f001e2fd2206da6f9782f832484ce8
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-29 22:53:42 +0330

    Update glitch.py

--- plugins/glitch.py
+++ plugins/glitch.py
@@ -1,17 +1,15 @@
 """
-
 ✘ Commands Available -
 
-•`{i}wglitch <replt to media>`
+•`{i}glitch <reply to media>`
     gives a glitchy gif.
-
 """
 import os
 
-from . import bash, eor, get_string, mediainfo, ultroid_cmd
+from . import bash, get_string, mediainfo, ultroid_cmd
 
 
-@ultroid_cmd(pattern="wglitch$")
+@ultroid_cmd(pattern="glitch$")
 async def _(e):
     try:
         import glitch_me  # ignore :pylint
@@ -20,16 +18,19 @@ async def _(e):
             "pip install -e git+https://github.com/1Danish-00/glitch_me.git#egg=glitch_me"
         )
     reply = await e.get_reply_message()
-    if not (reply and reply.media):
-        return await eor(e, "Reply to any media")
+    if not reply or not reply.media:
+        return await e.eor(get_string("cvt_3"))
+    xx = await e.eor(get_string("glitch_1"))
     wut = mediainfo(reply.media)
-    if not wut.startswith(("pic", "sticker")):
-        return await eor(e, get_string("com_4"))
-    xx = await eor(e, "`Gliching...`")
-    ok = await e.client.download_media(reply.media)
-    cmd = f"glitch_me gif --line_count 200 -f 10 -d 50 '{ok}' cipherx.gif"
-    stdout, stderr = await bash(cmd)
-    await e.reply(file="cipherx.gif", force_document=False)
+    if wut.startswith(("pic", "sticker")):
+        ok = await reply.download_media()
+    elif reply.document and reply.document.thumbs:
+        ok = await reply.download_media(thumb=-1)
+    else:
+        return await xx.eor(get_string("com_4"))
+    cmd = f"glitch_me gif --line_count 200 -f 10 -d 50 '{ok}' ult.gif"
+    await bash(cmd)
+    await e.reply(file="ult.gif", force_document=False)
     await xx.delete()
     os.remove(ok)
-    os.remove("cipherx.gif")
+    os.remove("ult.gif")
