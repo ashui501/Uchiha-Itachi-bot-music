c97cbf9ca525e49881c3836ea2dc7bb5a11fa662
Heartless <89603031+DarkPentester@users.noreply.github.com>
2023-03-29 22:21:23 +0330

    Update extra.py

--- plugins/extra.py
+++ plugins/extra.py
@@ -1,52 +1,22 @@
-"""
-✘ Commands Available -
+from . import get_help
 
-• `{i}del <reply to message>`
-    Delete the replied message.
+__doc__ = get_help("extra")
 
-• `{i}edit <new message>`
-    Edit your last message or replied msg.
-
-• `{i}copy <reply to message>`
-    Copy replied message / media.
-
-• `{i}reply`
-    Reply the last sent msg to replied user.
-"""
 import asyncio
 
-from telethon.events import NewMessage as NewMsg
-
-from . import HNDLR, eor, get_string, ultroid_bot, ultroid_cmd
-
-_new_msgs = {}
-
-
-@ultroid_bot.on(
-    NewMsg(
-        outgoing=True,
-    ),
-)
-async def newmsg(event):
-    if event.message.message == f"{HNDLR}reply":
-        return
-    _new_msgs[event.chat_id] = event.message
+from . import get_string, ultroid_cmd
 
 
 @ultroid_cmd(
     pattern="del$",
-    type=["official", "manager"],
+    manager=True,
 )
 async def delete_it(delme):
     msg_src = await delme.get_reply_message()
-    if msg_src:
-        try:
-            await msg_src.delete()
-            await delme.delete()
-        except Exception as e:
-            await eor(
-                delme, f"Couldn't delete the message.\n\n**ERROR:**\n`{e}`", time=5
-            )
+    if not msg_src:
+        return
+    await msg_src.try_delete()
+    await delme.try_delete()
 
 
 @ultroid_cmd(
@@ -56,8 +26,8 @@ async def copy(e):
     reply = await e.get_reply_message()
     if reply:
         await reply.reply(reply)
-        return await e.delete()
-    await eor(e, get_string("ex_1"), time=5)
+        return await e.try_delete()
+    await e.eor(get_string("ex_1"), time=5)
 
 
 @ultroid_cmd(
@@ -76,7 +46,7 @@ async def editer(edit):
             pass
     else:
         i = 1
-        async for message in edit.client.iter_messages(chat, ultroid_bot.uid):
+        async for message in edit.client.iter_messages(chat, from_user="me", limit=2):
             if i == 2:
                 await message.edit(string)
                 await edit.delete()
@@ -88,9 +58,16 @@ async def editer(edit):
     pattern="reply$",
 )
 async def _(e):
-    if e.reply_to_msg_id and e.chat_id in _new_msgs:
-        msg = _new_msgs[e.chat_id]
-        chat = await e.get_input_chat()
+    if e.reply_to_msg_id:
+        chat = e.chat_id
+        try:
+            msg = (await e.client.get_messages(e.chat_id, limit=1, max_id=e.id))[0]
+        except IndexError:
+            return await e.eor(
+                "`You have previously sent no message to reply again...`", time=5
+            )
+        except BaseException as er:
+            return await e.eor(f"**ERROR:** `{er}`")
         await asyncio.wait(
             [
                 e.client.delete_messages(chat, [e.id, msg.id]),
@@ -98,4 +75,4 @@ async def _(e):
             ]
         )
     else:
-        await e.delete()
+        await e.try_delete()
