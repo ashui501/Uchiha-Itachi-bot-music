b286cc23a8f4396f3a3c7831e758f37b8abf0f08
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-03-30 23:36:50 +0330

    Update __init__.py

--- CythonX/__init__.py
+++ CythonX/__init__.py
@@ -46,18 +46,18 @@ if run_as_module:
     if USER_MODE:
         DUAL_MODE = False
 
-    if BOT_MODE:
-        if DUAL_MODE:
-            udB.del_key("DUAL_MODE")
-            DUAL_MODE = False
-        ultroid_bot = None
+    #if BOT_MODE:
+        #if DUAL_MODE:
+            #udB.del_key("DUAL_MODE")
+            #DUAL_MODE = False
+        #ultroid_bot = None
 
-        if not udB.get_key("BOT_TOKEN"):
-            LOGS.critical(
-                '"BOT_TOKEN" not Found! Please add it, in order to use "BOTMODE"'
-            )
+        #if not udB.get_key("BOT_TOKEN"):
+            #LOGS.critical(
+                #'"BOT_TOKEN" not Found! Please add it, in order to use "BOTMODE"'
+            #)
 
-            sys.exit()
+            #sys.exit()
     else:
         ultroid_bot = UltroidClient(
             validate_session(Var.SESSION, LOGS),
@@ -72,17 +72,17 @@ if run_as_module:
     else:
         asst = UltroidClient(None, bot_token=udB.get_key("BOT_TOKEN"), udB=udB)
 
-    if BOT_MODE:
-        ultroid_bot = asst
-        if udB.get_key("OWNER_ID"):
-            try:
-                ultroid_bot.me = ultroid_bot.run_in_loop(
-                    ultroid_bot.get_entity(udB.get_key("OWNER_ID"))
-                )
-            except Exception as er:
-                LOGS.exception(er)
-    elif not asst.me.bot_inline_placeholder and asst._bot:
-        ultroid_bot.run_in_loop(enable_inline(ultroid_bot, asst.me.username))
+    #if BOT_MODE:
+        #ultroid_bot = asst
+        #if udB.get_key("OWNER_ID"):
+            #try:
+                #ultroid_bot.me = ultroid_bot.run_in_loop(
+                    #ultroid_bot.get_entity(udB.get_key("OWNER_ID"))
+                #)
+            #except Exception as er:
+                #LOGS.exception(er)
+    #elif not asst.me.bot_inline_placeholder and asst._bot:
+        #ultroid_bot.run_in_loop(enable_inline(ultroid_bot, asst.me.username))
 
     vcClient = vc_connection(udB, ultroid_bot)
 
