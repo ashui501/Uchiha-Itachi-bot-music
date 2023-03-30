044f7f6b36837ecb5ac31879d9214ed5dea9ee20
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-03-30 21:07:06 +0330

    Update __init__.py

--- CythonX/__init__.py
+++ CythonX/__init__.py
@@ -16,7 +16,7 @@ if run_as_module:
 
     from .configs import Var
     from .startup import *
-    from .startup._database import UltroidDB
+    from .startup._database import CipherXDB
     from .startup.BaseClient import UltroidClient
     from .startup.connections import validate_session, vc_connection
     from .startup.funcs import _version_changes, autobot, enable_inline, update_envs
@@ -32,7 +32,7 @@ if run_as_module:
     _ult_cache = {}
     _ignore_eval = []
 
-    udB = UltroidDB()
+    udB = CipherXDB()
     update_envs()
 
     LOGS.info(f"Connecting to {udB.name}...")
