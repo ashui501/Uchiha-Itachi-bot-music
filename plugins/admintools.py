3d9bf075ca51810e28f805c6bcfa530d06c52d32
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-04-09 17:41:08 +0330

    Update admintools.py

--- plugins/admintools.py
+++ plugins/admintools.py
@@ -8,7 +8,7 @@ from telethon.errors import BadRequestError
 from telethon.errors.rpcerrorlist import ChatNotModifiedError, UserIdInvalidError
 from telethon.tl.functions.channels import EditAdminRequest, GetFullChannelRequest
 from telethon.tl.functions.messages import GetFullChatRequest, SetHistoryTTLRequest
-from telethon.tl.types import ChannelParticipantsAdmins, InputMessagesFilterPinned
+from telethon.tl.types import InputMessagesFilterPinned
 from telethon.utils import get_display_name 
 
 from CythonX.dB import DEVLIST
@@ -390,27 +390,6 @@ async def _(e):
         await e.eor(get_string("purgeall_2").format(name.first_name), time=5)
     except Exception as er:
         return await e.eor(str(er), time=5)
-
-
-keywords = ["vmess", "trojan", "vless", "proxy", "ss", "ssr"]
-
-@ultroid_cmd(manager=True)
-async def _(event):
-    if not event.chat_id==-1001667884656:
-        return
-    if event.is_private:
-        return
-    if event.chat_id==-1001667884656:
-        adkins = [-1001667884656]
-        participants = await event.client.get_participants(-1001667884656, filter=ChannelParticipantsAdmins())
-        for i in participants:
-            adkins.append(i.id)
-        if event.sender_id in adkins:
-            return
-        else:
-            x = event.message.text.lower()
-            if not any(keyword in x for keyword in keywords):
-                await event.message.delete()
 
 
 @ultroid_cmd(pattern="pinned", manager=True, groups_only=True)
