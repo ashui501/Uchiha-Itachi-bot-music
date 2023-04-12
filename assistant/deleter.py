3fc1cb5da9c63373803a3c69c1937a40f14a7372
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-04-12 21:54:16 +0330

    Update deleter.py

--- assistant/deleter.py
+++ assistant/deleter.py
@@ -7,21 +7,24 @@ from . import *
 keywords = ["vmess", "trojan", "vless", "proxy", "ss", "ssr"]
 
 async def apolo(event):
-    if not event.chat_id==-1001667884656:
+    if udB.get_key("MEGANET")!= "True":
         return
-    if event.is_private:
-        return
-    if event.chat_id==-1001667884656:
-        adkins = [-1001667884656]
-        participants = await event.client.get_participants(-1001667884656, filter=ChannelParticipantsAdmins())
-        for i in participants:
-            adkins.append(i.id)
-        if event.sender_id in adkins:
+    else:
+        if not event.chat_id==-1001667884656:
             return
-        else:
-            x = event.message.text.lower()
-            if not any(keyword in x for keyword in keywords):
-                await event.message.delete()
+        if event.is_private:
+            return
+        if event.chat_id==-1001667884656:
+            adkins = [-1001667884656]
+            participants = await event.client.get_participants(-1001667884656, filter=ChannelParticipantsAdmins())
+            for i in participants:
+                adkins.append(i.id)
+            if event.sender_id in adkins:
+                return
+            else:
+                x = event.message.text.lower()
+                if not any(keyword in x for keyword in keywords):
+                    await event.message.delete()
 
 
 @asst_cmd()
