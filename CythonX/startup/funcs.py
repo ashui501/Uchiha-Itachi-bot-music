ada820bc7c7576872c0590af05b738f442bbe168
CoderX <66735676+ToxygenX@users.noreply.github.com>
2023-05-29 16:36:15 +0330

    Update funcs.py

--- CythonX/startup/funcs.py
+++ CythonX/startup/funcs.py
@@ -37,7 +37,8 @@ from telethon.tl.types import (
 from telethon.utils import get_peer_id
 
 from .. import LOGS, ULTConfig
-from ..fns.helper import download_file, inline_mention, updater, Bash
+
+from ..fns.helper import *
 
 db_url = 0
 
